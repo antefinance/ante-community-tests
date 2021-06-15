c3b6abd5e6d4c63419910f816a8e5aaa76df6bfc
DeLaLuna <delaluna@ante.finance>
2021-06-15 00:00:51 -0500

    Initialize community repo with hardhat and AnteTest interfaces

---
+++ contracts/interfaces/AnteTest.sol
@@ -0,0 +1,35 @@
+// ┏━━━┓━━━━━┏┓━━━━━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┏━┓┃━━━━┏┛┗┓━━━━━━━━┃┏━━┛━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┗━┛┃┏━┓━┗┓┏┛┏━━┓━━━━┃┗━━┓┏┓┏━┓━┏━━┓━┏━┓━┏━━┓┏━━┓
+// ┃┏━┓┃┃┏┓┓━┃┃━┃┏┓┃━━━━┃┏━━┛┣┫┃┏┓┓┗━┓┃━┃┏┓┓┃┏━┛┃┏┓┃
+// ┃┃ ┃┃┃┃┃┃━┃┗┓┃┃━┫━┏┓━┃┃━━━┃┃┃┃┃┃┃┗┛┗┓┃┃┃┃┃┗━┓┃┃━┫
+// ┗┛ ┗┛┗┛┗┛━┗━┛┗━━┛━┗┛━┗┛━━━┗┛┗┛┗┛┗━━━┛┗┛┗┛┗━━┛┗━━┛
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+import "./IAnteTest.sol";
+
+// Abstract inheritable contract to supply syntactic sugar for Ante Test writing
+// Usage: contract AnteNewProtocolTest is AnteTest("String descriptor of test", "Protocol name") { ... }
+abstract contract AnteTest is IAnteTest() { 
+    address public override testAuthor;
+    string public override testName;
+    string public override protocolName;
+    address [] public override testedContracts;
+
+    // testedContracts and protocolName are optional parameters which should be set in the constructor of your AnteTest
+    constructor (string memory _testName) {
+        testAuthor = msg.sender;
+        testName = _testName;
+    }
+
+    function getTestedContracts() external view returns (address[] memory) {
+        return testedContracts;
+    }
+
+    function checkTestPasses() public override virtual returns (bool) {}
+}
---
+++ contracts/interfaces/IAnteTest.sol
@@ -0,0 +1,29 @@
+// ┏━━━┓━━━━━┏┓━━━━━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┏━┓┃━━━━┏┛┗┓━━━━━━━━┃┏━━┛━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┗━┛┃┏━┓━┗┓┏┛┏━━┓━━━━┃┗━━┓┏┓┏━┓━┏━━┓━┏━┓━┏━━┓┏━━┓
+// ┃┏━┓┃┃┏┓┓━┃┃━┃┏┓┃━━━━┃┏━━┛┣┫┃┏┓┓┗━┓┃━┃┏┓┓┃┏━┛┃┏┓┃
+// ┃┃ ┃┃┃┃┃┃━┃┗┓┃┃━┫━┏┓━┃┃━━━┃┃┃┃┃┃┃┗┛┗┓┃┃┃┃┃┗━┓┃┃━┫
+// ┗┛ ┗┛┗┛┗┛━┗━┛┗━━┛━┗┛━┗┛━━━┗┛┗┛┗┛┗━━━┛┗┛┗┛┗━━┛┗━━┛
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
+
+interface IAnteTest {
+    // override the auto-generated getter for testAuthor as a public var 
+    function testAuthor() external view returns (address); 
+
+    // override the auto-generated getter for protocolName as a public var
+    function protocolName() external view returns (string memory);
+
+    // override the auto-generated getter for testedContracts [] as a public var
+    function testedContracts(uint i) external view returns (address);
+
+    // override the auto-generated getter for testName as a public var
+    function testName() external view returns (string memory); 
+
+    // single bool function that inspects the invariant; usually True
+    function checkTestPasses() external returns (bool); 
+}
