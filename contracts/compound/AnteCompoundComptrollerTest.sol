d1c158c81ae0d9c13501cd5e14563c2d7de859c7
Waldo Waldo <>
2021-10-06 13:25:01 -0400

    removed checkpointer and bug fix

--- contracts/compound/AnteCompoundComptrollerTest.sol
+++ contracts/compound/AnteCompoundComptrollerTest.sol
@@ -24,13 +24,9 @@ contract AnteCompoundComptrollerTest is AnteTest("Compound comptroller doesn't l
     /// @notice minimum period after checkpointing before checkTestPasses call
     /// is allowed to fail
     uint32 public constant MIN_PERIOD = 12 hours;
-    /// @notice period after checkpoint taken in which only checkpointer
-    /// is able to fail the test
-    /// @dev this protects the verifier bounty from being frontrun by other challengers
-    uint32 public constant PROTECTED_PERIOD = 14 hours;
     /// @notice minimum interval between allowing subsequent checkpoints
     /// @dev prevents malicious stakers from preventing a failing test by calling checkpoint() repeatedly
-    uint32 public constant MIN_CHECKPOINT_INTERVAL = 24 hours;
+    uint32 public constant MIN_CHECKPOINT_INTERVAL = 48 hours;
 
     /// @notice maximum rate of COMP decrease per second allowed, set to 10,000 COMP/day
     /// @dev according to https://compound.finance/governance/comp as of 2021/10/05
@@ -43,8 +39,6 @@ contract AnteCompoundComptrollerTest is AnteTest("Compound comptroller doesn't l
     /// @notice COMP balance at last checkpoint
     uint256 public lastCompBalance;
 
-    /// @notice address which called checkpoint() to prime test
-    address public checkpointer;
     /// @notice Compound comptroller address
     address public constant COMPTROLLER = 0x3d9819210A31b4961b30EF54bE2aeD79B9c9Cd3B;
     /// @notice COMP token
@@ -57,35 +51,31 @@ contract AnteCompoundComptrollerTest is AnteTest("Compound comptroller doesn't l
         checkpoint();
     }
 
-    /// @notice convenience function to check if enough time has elapsed after previous checkpoint
-    /// to check COMP balance change
-    /// @return true is checkTestPasses() can currently fail, false otherwise
-    function testCallable() external view returns (bool) {
-        return (block.timestamp.sub(lastCheckpointTime) > MIN_PERIOD);
-    }
-
     /// @notice take checkpoint of current COMP balance
     function checkpoint() public {
         require(
             block.timestamp.sub(lastCheckpointTime) > MIN_CHECKPOINT_INTERVAL,
-            "Cannot call checkpoint more than once every 24 hours"
+            "Cannot call checkpoint more than once every 48 hours"
         );
 
         lastCheckpointTime = block.timestamp;
         lastCompBalance = COMP.balanceOf(COMPTROLLER);
-        checkpointer = msg.sender;
     }
 
+    /// @notice checks that COMP balance of comptroller doesn't decrease by more than 10,000 COMP/day
+    /// @dev returns true if less than MIN_PERIOD has passed since last checkpoint or if COMP balance
+    /// increases to avoid reversion
+    /// @return true if comptroller COMP balance increases or deosn't decrease by less than 10,000 COMP/day
     function checkTestPasses() public view override returns (bool) {
         uint256 timeSinceLastCheckpoint = block.timestamp.sub(lastCheckpointTime);
         if (timeSinceLastCheckpoint > MIN_PERIOD) {
-            if (msg.sender != checkpointer && timeSinceLastCheckpoint < PROTECTED_PERIOD) {
-                // only checkpointer can trigger failing test in protected period
+            uint256 compBalance = COMP.balanceOf(COMPTROLLER);
+            // if COMP was added to contract then return true to avoid reversion due to underflow
+            if (compBalance >= lastCompBalance) {
                 return true;
             }
 
-            return
-                COMP.balanceOf(COMPTROLLER).sub(lastCompBalance).div(timeSinceLastCheckpoint) > COMP_PER_SEC_THRESHOLD;
+            return lastCompBalance.sub(compBalance).div(timeSinceLastCheckpoint) < COMP_PER_SEC_THRESHOLD;
         }
 
         // if timeSinceLastCheckpoint is less than MIN_PERIOD just return true
