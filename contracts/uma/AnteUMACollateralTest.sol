b3f83d19c91f645e74d30de675af6aef556e654e
DeLaLuna <delaluna@ante.finance>
2022-01-06 23:46:26 -0600

    interface moved into ante test, unit test added

--- contracts/uma/AnteUMACollateralTest.sol
+++ contracts/uma/AnteUMACollateralTest.sol
@@ -9,12 +9,28 @@
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 // ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "../AnteTest.sol";
-//import "./LongShortPair.sol";
-import "./interfaces/ILongShortPair.sol";
+
+
+/// @title Interface for an UMA LongShortPair
+interface ILongShortPair {
+
+  /// @return Amount of collateral a pair of tokens is always redeemable for.
+  function collateralPerPair() external view returns (uint256);
+
+  /// @return The ERC20 token that is used as collateral
+  function collateralToken() external view returns (IERC20);
+
+  /// @return The ERC20 token used as the long for the LongShortPair.
+  function longToken() external view returns (IERC20);
+
+  /// @return The ERC20 token used as the short for the LongShortPair.
+  function shortToken() external view returns (IERC20);
+}
 
 
 /// @title Amount of collateral token locked equals long/short tokens minted times collateral
@@ -22,6 +38,7 @@ import "./interfaces/ILongShortPair.sol";
 contract AnteUMACollateralTest is AnteTest(
   "Collateral token locked matches Long and Short tokens minted times collateral"
 ) {
+  using SafeMath for uint256;
 
   /// @param umaLspAddr array of UMA LSP addresses to check against
   constructor(address[] memory umaLspAddr) {
@@ -32,11 +49,11 @@ contract AnteUMACollateralTest is AnteTest(
   /// @return true if all contracts have balanced minted tokens times collateral to locked collateral
   function checkTestPasses() public view override returns (bool) {
     for (uint256 i = 0; i < testedContracts.length; i++) {
-      LongShortPair umaLsp = ILongShortPair(testedContracts[i]);
-      uint256 collateral = umaLsp.collateralPerPair;
-      uint256 collateralLocked = IERC20(umaLsp.collateralToken).balanceOf(testedContracts[i]);
-      uint256 longTokensMinted = IERC20(umaLsp.longToken).totalSupply();
-      uint256 shortTokensMinted = IERC20(umaLsp.shortToken).totalSupply();
+      ILongShortPair umaLsp = ILongShortPair(testedContracts[i]);
+      uint256 collateral = umaLsp.collateralPerPair();
+      uint256 collateralLocked = umaLsp.collateralToken().balanceOf(testedContracts[i]);
+      uint256 longTokensMinted = umaLsp.longToken().totalSupply();
+      uint256 shortTokensMinted = umaLsp.shortToken().totalSupply();
       if (longTokensMinted != shortTokensMinted) {
         return false;
       }
