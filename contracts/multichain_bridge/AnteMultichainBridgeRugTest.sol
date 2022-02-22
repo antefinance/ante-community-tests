2fd6989056070999f37ed82128b90ec98d4c30d5
Allan Lin <allanlin@gmail.com>
2022-02-21 23:37:05 -0500

    Fixing Test to be more aligned with expectations.

--- contracts/multichain_bridge/AnteMultichainBridgeRugTest.sol
+++ contracts/multichain_bridge/AnteMultichainBridgeRugTest.sol
@@ -12,58 +12,53 @@ contract AnteMultichainBridgeRugTest is AnteTest("Top 5 assets do not lose 90% v
     // Externally Owned Account - https://etherscan.io/address/0x13B432914A996b0A48695dF9B2d701edA45FF264
     address public constant eoaAnyswapBSCBridgeAddr = 0x13B432914A996b0A48695dF9B2d701edA45FF264;
 
-    // Used https://anyswap.net/tokens sorted by TVL to determine the top 5 assets.
+    // Selecting the top 4 ERC-20 Tokens + ETH on Anyway Bridge
 
-    //Anyswap - https://etherscan.io/address/0xf99d58e463A2E07e5692127302C20A191861b4D6
-    address public constant anyswapAddr = 0xf99d58e463A2E07e5692127302C20A191861b4D6;
+    //TRVL - https://etherscan.io/address/0xd47bDF574B4F76210ed503e0EFe81B58Aa061F3d
+    address public constant trvlAddr = 0xd47bDF574B4F76210ed503e0EFe81B58Aa061F3d;
 
-    //Fanthom - https://etherscan.io/address/0x4e15361fd6b4bb609fa63c81a2be19d873717870
-    address public constant ftmAddr = 0x4E15361FD6b4BB609Fa63C81A2be19d873717870;
+    //Super - https://etherscan.io/address/0xe53EC727dbDEB9E2d5456c3be40cFF031AB40A55
+    address public constant superAddr = 0xe53EC727dbDEB9E2d5456c3be40cFF031AB40A55;
 
-    //USDC - https://etherscan.io/address/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
-    address public constant usdcAddr = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    //mtlx - https://etherscan.io/address/0x2e1E15C44Ffe4Df6a0cb7371CD00d5028e571d14
+    address public constant mtlxAddr = 0x2e1E15C44Ffe4Df6a0cb7371CD00d5028e571d14;
 
     //TetherUSD - https://etherscan.io/address/0xdAC17F958D2ee523a2206206994597C13D831ec7
     address public constant tetherAddr = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-    //DaiStablecoin - https://etherscan.io/address/0x6B175474E89094C44Da98b954EedeAC495271d0F
-    address public constant daiAddr = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-
-    IERC20 public anyswapToken;
-    IERC20 public ftmToken;
-    IERC20 public usdcToken;
+    IERC20 public trvlToken;
+    IERC20 public superToken;
+    IERC20 public mtlxToken;
     IERC20 public tetherToken;
-    IERC20 public daiToken;
-    uint256 public immutable anyswapBalanceAtDeploy;
-    uint256 public immutable ftmBalanceAtDeploy;
-    uint256 public immutable usdcBalanceAtDeploy;
-    uint256 public immutable tehterBalanceAtDeploy;
-    uint256 public immutable daiBalanceAtDeploy;
+
+    uint256 public immutable trvlBalanceAtDeploy;
+    uint256 public immutable superBalanceAtDeploly;
+    uint256 public immutable mtlxBalanceAtDeploy;
+    uint256 public immutable tetherBalanceAtDeploy;
+    uint256 public immutable ethBalanceAtDeploy;
 
     constructor() {
         protocolName = "Anyswap: BSC Bridge";
         testedContracts = [eoaAnyswapBSCBridgeAddr];
 
-        anyswapToken = IERC20(anyswapAddr);
-        ftmToken = IERC20(ftmAddr);
-        usdcToken = IERC20(usdcAddr);
+        trvlToken = IERC20(trvlAddr);
+        superToken = IERC20(superAddr);
+        mtlxToken = IERC20(mtlxAddr);
         tetherToken = IERC20(tetherAddr);
-        daiToken = IERC20(daiAddr);
 
-        // Snapshot of top 5 tokens.
-        anyswapBalanceAtDeploy = anyswapToken.balanceOf(eoaAnyswapBSCBridgeAddr);
-        ftmBalanceAtDeploy = ftmToken.balanceOf(eoaAnyswapBSCBridgeAddr);
-        usdcBalanceAtDeploy = usdcToken.balanceOf(eoaAnyswapBSCBridgeAddr);
-        tehterBalanceAtDeploy = tetherToken.balanceOf(eoaAnyswapBSCBridgeAddr);
-        daiBalanceAtDeploy = daiToken.balanceOf(eoaAnyswapBSCBridgeAddr);
+        trvlBalanceAtDeploy = trvlToken.balanceOf(eoaAnyswapBSCBridgeAddr);
+        superBalanceAtDeploly = superToken.balanceOf(eoaAnyswapBSCBridgeAddr);
+        mtlxBalanceAtDeploy = mtlxToken.balanceOf(eoaAnyswapBSCBridgeAddr);
+        tetherBalanceAtDeploy = tetherToken.balanceOf(eoaAnyswapBSCBridgeAddr);
+        ethBalanceAtDeploy = address(eoaAnyswapBSCBridgeAddr).balance;
     }
 
     function checkTestPasses() public view override returns (bool) {
         return
-            anyswapBalanceAtDeploy * 90 <= anyswapToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 100 &&
-            ftmBalanceAtDeploy * 90 <= ftmToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 100 &&
-            usdcBalanceAtDeploy * 90 <= usdcToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 100 &&
-            tehterBalanceAtDeploy * 90 <= tetherToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 100 &&
-            daiBalanceAtDeploy * 90 <= daiToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 100;
+            trvlBalanceAtDeploy <= trvlToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 10 &&
+            superBalanceAtDeploly <= superToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 10 &&
+            mtlxBalanceAtDeploy <= mtlxToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 10 &&
+            tetherBalanceAtDeploy <= tetherToken.balanceOf(eoaAnyswapBSCBridgeAddr) * 10 &&
+            ethBalanceAtDeploy <= address(eoaAnyswapBSCBridgeAddr).balance * 10;
     }
 }
