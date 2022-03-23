cc3c26e3d8a5f1638a6438a757ddac7d36d5d030
Anton <62949848+icepaq@users.noreply.github.com>
2022-03-23 12:31:16 -0400

    update name of contract

--- test/lidoinsurance/ante_lido_insurance_test.spec.ts
+++ test/lidoinsurance/ante_lido_insurance_test.spec.ts
@@ -15,7 +15,7 @@ describe('LidoInsuranceTest', function () {
     globalSnapshotId = await evmSnapshot();
 
     const [deployer] = waffle.provider.getWallets();
-    const factory = (await hre.ethers.getContractFactory('LidoInsuranceTest', deployer)) as AnteLidoInsuranceTest__factory;
+    const factory = (await hre.ethers.getContractFactory('AnteLidoInsuranceTest', deployer)) as AnteLidoInsuranceTest__factory;
     test = await factory.deploy('0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84', '0x3e40D73EB977Dc6a537aF587D48316feE66E9C8c');
     await test.deployed();
   });
