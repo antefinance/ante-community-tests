b25deecee4c0ba7a4ba4bb922743b8e95f4b2f8e
Anton <62949848+icepaq@users.noreply.github.com>
2022-04-04 23:51:00 -0400

    initial commit

---
+++ contracts/uniswap/Ante_USDT-UST_Slippage_Test.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "../AnteTest.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+
+// @title Ensure that the slippage of these two tokens are <= 3%
+contract AnteUSDTUSTSlippage is AnteTest("Ante UST-USDT Slippage Test on Uniswap") {
+    // https://etherscan.io/token/0xc50Ef7861153C51D383d9a7d48e6C9467fB90c38
+    address public constant USDT_UST_Pair = 0xc50Ef7861153C51D383d9a7d48e6C9467fB90c38;
+
+    constructor() {
+        protocolName = "Uniswap";
+        testedContracts = [USDT_UST_Pair];
+    }
+    
+    // @return reserve0 and reserve1 from the uniswap pair
+    // @notice Will only work on mainnet
+    function getTokenPrice() public view returns(uint112, uint112) {
+        IUniswapV2Pair uniswapPair = IUniswapV2Pair(USDT_UST_Pair);
+
+        (uint112 x, uint112 y,) = uniswapPair.getReserves();
+
+        // Always make sure that the percentage will be equal to or less than 100
+        if(x > y) {
+            (x, y) = (y, x); // More gas efficient swap compared to using a temp var
+        }
+        return (x, y);
+    }
+
+    // @return bool if the price difference is >= 3%
+    function checkTestPasses() public view override returns (bool) {
+        (uint112 x, uint112 y) = getTokenPrice();
+
+        return(100 * x / y >= 97);
+    }
+}
