cfeb115cc644a4449ab87f50674e42a37ab7f44a
Anton <62949848+icepaq@users.noreply.github.com>
2022-04-07 14:55:58 -0400

    use IERC20

--- contracts/uniswap/AnteUniswapLiquidityTVLTest.sol
+++ contracts/uniswap/AnteUniswapLiquidityTVLTest.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "../AnteTest.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../interfaces/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /// @title Ensure that the liquidity TVL of USDC/ETH and USDT/ETH does not have a difference greater than 10x
@@ -15,9 +15,9 @@ contract AnteUniswapUSDCETHUSDTETHPoolTVLDifference is AnteTest("Make sure that 
     address private constant ETH_USDT = 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36;
     address private constant ETH_USDC = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
 
-    ERC20 private constant USDT = ERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
-    ERC20 private constant USDC = ERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    ERC20 private constant WETH = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 private constant USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    IERC20 private constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IERC20 private constant WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
     AggregatorV3Interface private ethPriceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
 
