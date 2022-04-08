f6f5e7387d4b74bd761b85a4bfc019fe48b489c7
Anton <62949848+icepaq@users.noreply.github.com>
2022-04-08 05:05:55 -0400

    comment for uniswap pool & update variable name for pool contract

--- contracts/uniswap/AnteUniswapLiquidityTVLTest.sol
+++ contracts/uniswap/AnteUniswapLiquidityTVLTest.sol
@@ -12,8 +12,9 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 /// @notice Uses AggregatorV3Interface to calculate WETH price
 contract AnteUniswapUSDCETHUSDTETHPoolTVLDifference is AnteTest("Make sure that TVL of USDC/ETH & USDT/ETH liquidity pools is not 10x or greater of the smaller pool") {
 
-    address private constant ETH_USDT = 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36;
-    address private constant ETH_USDC = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
+    // Uniswap V3 Pools
+    address private constant WETH_USDT = 0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36;
+    address private constant WETH_USDC = 0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8;
 
     IERC20 private constant USDT = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
     IERC20 private constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
@@ -23,7 +24,7 @@ contract AnteUniswapUSDCETHUSDTETHPoolTVLDifference is AnteTest("Make sure that 
 
     constructor() {
         protocolName = "Uniswap";
-        testedContracts = [ETH_USDT, ETH_USDC];
+        testedContracts = [WETH_USDT, WETH_USDC];
     }
 
     /// @notice Takes the TVL of two liquidity pools. USDC/ETH and USDT/ETH
@@ -36,14 +37,14 @@ contract AnteUniswapUSDCETHUSDTETHPoolTVLDifference is AnteTest("Make sure that 
         uint256 ethToUSD = uint256(signedethToUSD);
 
         // ETH/USDC TVL
-        uint256 ethusdcUSDC = USDC.balanceOf(ETH_USDC);
-        uint256 ethusdcWETH = WETH.balanceOf(ETH_USDC) / (10 ** 12); // Make up for decimal difference
+        uint256 ethusdcUSDC = USDC.balanceOf(WETH_USDC);
+        uint256 ethusdcWETH = WETH.balanceOf(WETH_USDC) / (10 ** 12); // Make up for decimal difference
 
         uint256 ethusdcDollarValue = ethusdcUSDC + (ethusdcWETH * ethToUSD);
 
         // ETH/USDT TVL
-        uint256 ethusdtUSDT = USDT.balanceOf(ETH_USDT);
-        uint256 ethusdtWETH = WETH.balanceOf(ETH_USDT) / (10 ** 12); // Make up for decimal difference
+        uint256 ethusdtUSDT = USDT.balanceOf(WETH_USDT);
+        uint256 ethusdtWETH = WETH.balanceOf(WETH_USDT) / (10 ** 12); // Make up for decimal difference
 
         uint256 ethusdtDollarValue = ethusdtUSDT + (ethusdtWETH * ethToUSD);
 
