8683e718640a0edd246995784c317c2fd60d1f09
Anton <62949848+icepaq@users.noreply.github.com>
2022-04-08 12:22:02 -0400

    license identifier, update etherscan comment

--- contracts/opyn/AnteOpynTVLPlungeTest.sol
+++ contracts/opyn/AnteOpynTVLPlungeTest.sol
@@ -1,10 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "../AnteTest.sol";
 
 /// @notice Checks if the TVL is at least 15% the original TVL
 contract AnteOpynPlungeTest is AnteTest("Opyn Balance is greater than 15% the original") {
-    // https://etherscan.io/token/0xdAC17F958D2ee523a2206206994597C13D831ec7
+    // https://etherscan.io/token/0x64187ae08781B09368e6253F9E94951243A493D5
     address private constant OPYN_ADDRESS = 0x64187ae08781B09368e6253F9E94951243A493D5;
 
     uint256 private immutable oldBalance;
