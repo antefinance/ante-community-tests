0010b7f981080f8b7af873c20099dac07d3e91bf
Anton <62949848+icepaq@users.noreply.github.com>
2022-04-27 23:03:25 -0400

    Yearn Withdraw Test

---
+++ contracts/yfi/AnteYearnWithdrawTest.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+import "../AnteTest.sol";
+import "../interfaces/IERC20.sol";
+
+interface IYearnVault {
+    function withdraw() external;
+    function balanceOf(address account) external view returns (uint256);
+}
+
+/// @title Yearn Withdraw Test
+/// @notice Tests to see if you can withdraw from a yearn deposit
+contract AnteYearnWithdrawTest is AnteTest("Makes sure you can withdraw from yearn") {
+
+    address public immutable vault;
+    address public immutable token;
+
+    IYearnVault private immutable yearnVault;
+    IERC20 private immutable tokenContract;
+
+    uint256 private constant DAY_IN_SECONDS = 86400;
+    uint256 private constant FIFTEEEN_MINUTES_IN_SECONDS = 15 * 60;
+
+    uint256 private tokenBalance = 0;
+    uint256 private yearnBalance = 0;
+    uint256 private withdrawingEpoch = 0;
+
+    /// @param _vault The address of the yearn vault contract
+    /// @param _token The address of the token contract associated with the vault
+    constructor(address _vault, address _token) {
+        protocolName = "Yearn";
+        testedContracts = [_vault];
+
+        vault = _vault;
+        token = _token;
+        yearnVault = IYearnVault(vault);
+        tokenContract = IERC20(token);
+    }
+
+    /// @notice Withdraws tokens from yearn vault
+    /// @dev Vault must have tokens and can only be called once per day
+    /// @dev token balance - will be checked to see if value is higher in the future
+    /// @dev yearn balance - will be checked to see if value is not zero. If value is zero, test will return true
+    /// @dev epoch - used to ensure that some time has passed
+    function withdraw() external {
+        require(yearnVault.balanceOf(address(this)) > 0, "You have no tokens to withdraw");
+        require(block.timestamp - withdrawingEpoch > DAY_IN_SECONDS, "You can only withdraw once per day");
+        
+        tokenBalance = tokenContract.balanceOf(address(this));
+        yearnBalance = yearnVault.balanceOf(address(this));
+        withdrawingEpoch = block.timestamp;
+
+        yearnVault.withdraw();
+    }
+
+    /// @dev Must call withdraw() prior to calling this function. Or else test will always be true.
+    /// @dev Must wait for 15 minutes after calling withdraw()
+    /// @return if tokens have been withdrawn
+    function checkTestPasses() public view override returns (bool) {
+        // If yearn balance is zero, that means that either withdraw() was never called or 
+        // when it was called, the contract didn't own anything in the vault.
+        if(yearnBalance == 0) {
+            return true;
+        }
+
+        // It may take some time to process transactions from the yearn vault back to the stablecoin contract
+        if(block.timestamp - withdrawingEpoch < FIFTEEEN_MINUTES_IN_SECONDS) {
+            return true;
+        }
+
+        return(tokenContract.balanceOf(address(this)) > tokenBalance);
+    }
+}
---
+++ test/yfi/AnteYearnWithdrawTest.spec.ts
@@ -0,0 +1,35 @@
+import hre from 'hardhat';
+const { waffle } = hre;
+
+import { AnteYearnWithdrawTest, AnteYearnWithdrawTest__factory } from '../../typechain';
+
+import { evmSnapshot, evmRevert } from '../helpers';
+import { expect } from 'chai';
+
+
+describe('AnteYearnWithdrawTest', function () {
+  let test: AnteYearnWithdrawTest;
+
+  let globalSnapshotId: string;
+
+  const [deployer] = waffle.provider.getWallets();
+  const USDC_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+  const yUSDC_VAULT = "0x5f18c75abdae578b483e5f43f12a39cf75b973a9";
+
+
+  before(async () => {
+    globalSnapshotId = await evmSnapshot();
+    const factory = (await hre.ethers.getContractFactory('AnteYearnWithdrawTest', deployer)) as AnteYearnWithdrawTest__factory;
+    test = await factory.deploy(yUSDC_VAULT, USDC_ADDRESS);
+    await test.deployed();
+  });
+
+  after(async () => {
+    await evmRevert(globalSnapshotId);
+  });
+
+  it('should pass', async () => {
+    await test.withdraw();
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+});
