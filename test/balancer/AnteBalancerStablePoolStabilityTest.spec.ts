8957cf636f56987e845b3f551a2406186ed4306f
Anton <62949848+icepaq@users.noreply.github.com>
2022-04-29 01:48:14 -0400

    Balancer Stable Pool Balance Test

---
+++ test/balancer/AnteBalancerStablePoolStabilityTest.spec.ts
@@ -0,0 +1,51 @@
+import hre, { ethers } from 'hardhat';
+const { waffle } = hre;
+
+import { AnteBalancerStablePoolStabilityTest, AnteBalancerStablePoolStabilityTest__factory } from '../../typechain';
+
+import { evmSnapshot, evmRevert } from '../helpers';
+import { expect } from 'chai';
+import { formatBytes32String } from 'ethers/lib/utils';
+
+describe('AnteBalancerStablePoolStabilityTest', function () {
+  let test: AnteBalancerStablePoolStabilityTest;
+
+  let globalSnapshotId: string;
+
+  before(async () => {
+    globalSnapshotId = await evmSnapshot();
+
+    const [deployer] = waffle.provider.getWallets();
+    const factory = (await hre.ethers.getContractFactory('AnteBalancerStablePoolStabilityTest', deployer)) as AnteBalancerStablePoolStabilityTest__factory;
+    test = await factory.deploy('0x06df3b2bbb68adc8b0e302443692037ed9f91b42000000000000000000000063');
+    await test.deployed();
+  });
+
+  after(async () => {
+    await evmRevert(globalSnapshotId);
+  });
+
+  it('ratioValid should return true ', async () => {
+    expect(await test.ratioValid('100', '33', '3')).to.be.true;
+    expect(await test.ratioValid('100', '36', '3')).to.be.true;
+    expect(await test.ratioValid('100', '30', '3')).to.be.true;
+
+    expect(await test.ratioValid('160', '40', '4')).to.be.true;
+    expect(await test.ratioValid('160', '43', '4')).to.be.true;
+    expect(await test.ratioValid('160', '37', '4')).to.be.true;
+  });
+
+  it('ratioValid should return false ', async () => {
+    expect(await test.ratioValid('100', '37', '3')).to.be.false
+    expect(await test.ratioValid('100', '29', '3')).to.be.false;
+
+    expect(await test.ratioValid('160', '47', '4')).to.be.false;
+    expect(await test.ratioValid('160', '34', '4')).to.be.false;
+  });
+  
+  
+  it('should pass', async () => {
+    console.log((await test.ratioValid('100', '33', '3')).toString());
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+});
