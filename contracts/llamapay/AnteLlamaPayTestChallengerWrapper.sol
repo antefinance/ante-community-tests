92ce533de5e1c63944ea7f507c3c0eed9f321950
A bit whale-ish <85895672+abitwhaleish@users.noreply.github.com>
2022-05-11 15:58:19 -0700

    added test address on eth mainnet

--- contracts/llamapay/AnteLlamaPayTestChallengerWrapper.sol
+++ contracts/llamapay/AnteLlamaPayTestChallengerWrapper.sol
@@ -36,7 +36,7 @@ interface IAnteLlamaPayTest {
 ///         test. This works by allowing the user to set test parameters and
 ///         verify the test in the same transaction.
 contract AnteLlamaPayTestChallengerWrapper is Ownable {
-    // https://etherscan.io/address/[LLAMAPAY_ANTE_TEST_ADDRESS]
+    // https://etherscan.io/address/0x62ca84def073e6788b4f68e387617e50c8d36ebf
     IAnteLlamaPayTest public immutable test;
 
     // https://etherscan.io/address/[LLAMAPAY_ANTE_POOL_ADDRESS]
