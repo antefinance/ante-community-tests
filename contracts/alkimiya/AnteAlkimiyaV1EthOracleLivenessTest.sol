070cca51526e8599bfff17657c0f5e9e6750d98f
abitwhaleish <abitwhaleish@ante.finance>
2022-05-24 15:19:28 -0700

    draft alkimiya oracle liveness test

---
+++ contracts/alkimiya/AnteAlkimiyaV1EthOracleLivenessTest.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+// ┏━━━┓━━━━━┏┓━━━━━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┏━┓┃━━━━┏┛┗┓━━━━━━━━┃┏━━┛━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┗━┛┃┏━┓━┗┓┏┛┏━━┓━━━━┃┗━━┓┏┓┏━┓━┏━━┓━┏━┓━┏━━┓┏━━┓
+// ┃┏━┓┃┃┏┓┓━┃┃━┃┏┓┃━━━━┃┏━━┛┣┫┃┏┓┓┗━┓┃━┃┏┓┓┃┏━┛┃┏┓┃
+// ┃┃ ┃┃┃┃┃┃━┃┗┓┃┃━┫━┏┓━┃┃━━━┃┃┃┃┃┃┃┗┛┗┓┃┃┃┃┃┗━┓┃┃━┫
+// ┗┛ ┗┛┗┛┗┛━┗━┛┗━━┛━┗┛━┗┛━━━┗┛┗┛┗┛┗━━━┛┗┛┗┛┗━━┛┗━━┛
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+pragma solidity ^0.7.0;
+
+import "../libraries/ante-v05-core/AnteTest.sol";
+
+interface IOracle {
+    function get(uint256 _referenceDay)
+        external
+        view
+        returns (
+            uint256 referenceDay,
+            uint256 referenceBlock,
+            uint256 hashrate,
+            uint256 reward,
+            uint256 fees,
+            uint256 difficulty,
+            uint256 timestamp
+        );
+
+    function getLastIndexedDay() external view returns (uint32);
+}
+
+/// @title  Alkimiya V1 ETH oracle never goes >72 hrs without an update
+/// @notice Ante Test to check that no more than 72 hours has passed since
+///         the last oracle update
+contract AnteAlkimiyaV1EthOracleLivenessTest is
+    AnteTest("Alkimiya V1 ETH oracle never goes >72 hrs without an update")
+{
+    // https://snowtrace.io/address/0x3cb3608bff641b55f8dbafe86afc91cd36a17185 on Avax C-Chain
+    IOracle internal oracle;
+
+    constructor(address _oracleAddress) {
+        oracle = IOracle(_oracleAddress);
+
+        protocolName = "Alkimiya"; // <3
+        testedContracts.push(_oracleAddress);
+    }
+
+    /// @notice Checks that the last update for the Alkimiya V1 ETH oracle was
+    ///         at most 72 hours ago
+    /// @return true if it has been less than 72 hours since the last oracle update
+    function checkTestPasses() external view override returns (bool) {
+        // get timestamp of last update
+        (, , , , , , uint256 timestamp) = oracle.get(oracle.getLastIndexedDay());
+
+        // assume that timestamp <= block.timestamp
+        return block.timestamp - timestamp < 72 hours;
+    }
+}
---
+++ test/alkimiya/ante_alkimiya_oracle_liveness_test.spec.ts
@@ -0,0 +1,39 @@
+import hre from 'hardhat';
+const { waffle } = hre;
+
+import { AnteAlkimiyaV1EthOracleLivenessTest, AnteAlkimiyaV1EthOracleLivenessTest__factory } from '../../typechain';
+
+import { evmSnapshot, evmRevert, evmIncreaseTime } from '../helpers';
+import { expect } from 'chai';
+
+describe.only('AnteAlkimiyaV1EthOracleLivenessTest', function () {
+  let test: AnteAlkimiyaV1EthOracleLivenessTest;
+
+  let globalSnapshotId: string;
+
+  before(async () => {
+    globalSnapshotId = await evmSnapshot();
+
+    const [deployer] = waffle.provider.getWallets();
+    const factory = (await hre.ethers.getContractFactory(
+      'AnteAlkimiyaV1EthOracleLivenessTest',
+      deployer
+    )) as AnteAlkimiyaV1EthOracleLivenessTest__factory;
+    test = await factory.deploy('0x3CB3608bfF641b55F8DBaFe86AFC91Cd36a17185');
+    await test.deployed();
+  });
+
+  after(async () => {
+    await evmRevert(globalSnapshotId);
+  });
+
+  it('should pass', async () => {
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('if advance 72 hrs, test should fail', async () => {
+    // advance time 72 hrs
+    await evmIncreaseTime(259200);
+    await expect(test.checkTestPasses()).to.be.false;
+  });
+});
