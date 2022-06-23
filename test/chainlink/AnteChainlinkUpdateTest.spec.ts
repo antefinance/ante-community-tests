923b56f938658a97741cc439a9b961f02008c65e
Daniel Isac <43569886+danielcrt@users.noreply.github.com>
2022-06-23 07:45:21 +0300

    Aave TVL plunge test (#92)
    * add aave plunge test
    
    * fix mistaken rename

--- test/chainlink/AnteChainlinkUpdateTest.spec.ts
+++ test/chainlink/AnteChainlinkUpdateTest.spec.ts
@@ -15,8 +15,14 @@ describe('AnteChainlinkUpdateTimeTest', function () {
     globalSnapshotId = await evmSnapshot();
 
     const [deployer] = waffle.provider.getWallets();
-    const factory = (await hre.ethers.getContractFactory('AnteChainlinkUpdateTimeTest', deployer)) as AnteChainlinkUpdateTimeTest__factory;
-    test = await factory.deploy(['0x833D8Eb16D306ed1FbB5D7A2E019e106B960965A', '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419']);
+    const factory = (await hre.ethers.getContractFactory(
+      'AnteChainlinkUpdateTimeTest',
+      deployer
+    )) as AnteChainlinkUpdateTimeTest__factory;
+    test = await factory.deploy([
+      '0x833D8Eb16D306ed1FbB5D7A2E019e106B960965A',
+      '0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419',
+    ]);
     await test.deployed();
   });
 
