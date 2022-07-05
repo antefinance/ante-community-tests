07c68093c518b01382d8d35ece6ede60fafcb20c
abitwhaleish <abitwhaleish@ante.finance>
2022-07-05 15:24:04 -0700

    unit test for alkimiya v2 oracle liveness test

---
+++ test/alkimiya/ante_alkimiya_v2_eth_oracle_liveness_test.spec.ts
@@ -0,0 +1,43 @@
+import hre from 'hardhat';
+const { waffle } = hre;
+
+import {
+  AnteAlkimiyaV2EthOracleLivenessTestAvax,
+  AnteAlkimiyaV2EthOracleLivenessTestAvax__factory,
+} from '../../typechain';
+
+import { evmSnapshot, evmRevert, evmIncreaseTime, evmMineBlocks } from '../helpers';
+import { expect } from 'chai';
+
+describe.only('AnteAlkimiyaV2EthOracleLivenessTest', function () {
+  let test: AnteAlkimiyaV2EthOracleLivenessTestAvax;
+
+  let globalSnapshotId: string;
+
+  before(async () => {
+    globalSnapshotId = await evmSnapshot();
+
+    const [deployer] = waffle.provider.getWallets();
+    const factory = (await hre.ethers.getContractFactory(
+      'AnteAlkimiyaV2EthOracleLivenessTest',
+      deployer
+    )) as AnteAlkimiyaV2EthOracleLivenessTestAvax__factory;
+    test = await factory.deploy('0x3CB3608bfF641b55F8DBaFe86AFC91Cd36a17185');
+    await test.deployed();
+  });
+
+  after(async () => {
+    await evmRevert(globalSnapshotId);
+  });
+
+  it('should pass', async () => {
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('if advance 72 hrs, test should fail', async () => {
+    // advance time 72 hrs and 1 block
+    await evmIncreaseTime(259200);
+    await evmMineBlocks(1);
+    expect(await test.checkTestPasses()).to.be.false;
+  });
+});
