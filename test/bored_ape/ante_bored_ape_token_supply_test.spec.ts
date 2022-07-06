b6805b4d911fa29c95f29b16a5eb338b69119596
George Tamer <gtamer2@gmail.com>
2022-07-05 23:54:40 -0400

    Add a failure test

--- test/bored_ape/ante_bored_ape_token_supply_test.spec.ts
+++ test/bored_ape/ante_bored_ape_token_supply_test.spec.ts
@@ -1,19 +1,25 @@
 import hre from 'hardhat';
 const { waffle } = hre;
 
-import { AnteBoredApeMaxSupplyTest, AnteBoredApeMaxSupplyTest__factory } from '../../typechain';
+// TODO: Fix this IERC721 import not working
+import { IERC721, AnteBoredApeMaxSupplyTest, AnteBoredApeMaxSupplyTest__factory } from '../../typechain';
 
-import { evmSnapshot, evmRevert } from '../helpers';
+import { evmSnapshot, evmRevert, runAsSigner } from '../helpers';
 import { expect } from 'chai';
 
 describe('AnteBoredApeMaxSupplyTest', function () {
   let test: AnteBoredApeMaxSupplyTest;
-
+  let bayc: IERC721;
   let globalSnapshotId: string;
 
   before(async () => {
     globalSnapshotId = await evmSnapshot();
-
+    bayc = <IERC721>(
+      await hre.ethers.getContractAt(
+        '@openzeppelin/contracts/token/ERC721/IERC721.sol:IERC721',
+        '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
+      )
+    );
     const [deployer] = waffle.provider.getWallets();
     const factory = (await hre.ethers.getContractFactory(
       'AnteBoredApeMaxSupplyTest',
@@ -29,5 +35,17 @@ describe('AnteBoredApeMaxSupplyTest', function () {
 
   it('should pass', async () => {
     expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('should fail if Yuga Labs reserves apes', async () => {
+    const BAYC_DEPLOYER_ADDRESS = '0xaBA7161A7fb69c88e16ED9f455CE62B791EE4D03';
+
+    await runAsSigner(BAYC_DEPLOYER_ADDRESS, async () => {
+      const yugaLabs = await hre.ethers.getSigner(BAYC_DEPLOYER_ADDRESS);
+
+      // mint 30 new apes for yuga, bringing total above MAX
+      await bayc.connect(yugaLabs).reserveApes();
+    });
+    expect(await test.checkTestPasses()).to.be.false;
   });
 });
