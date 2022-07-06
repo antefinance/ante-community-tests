56d5ebb34cb5aeeb4e2555690a60aa7164b47e50
George Tamer <gtamer2@gmail.com>
2022-07-06 00:13:47 -0400

    Fix interface issue in test

--- test/bored_ape/ante_bored_ape_token_supply_test.spec.ts
+++ test/bored_ape/ante_bored_ape_token_supply_test.spec.ts
@@ -1,20 +1,19 @@
 import hre from 'hardhat';
 const { waffle } = hre;
 
-// TODO: Fix this IERC721 import not working
-import { IERC721, AnteBoredApeMaxSupplyTest, AnteBoredApeMaxSupplyTest__factory } from '../../typechain';
+import { BoredApes, AnteBoredApeMaxSupplyTest, AnteBoredApeMaxSupplyTest__factory } from '../../typechain';
 
 import { evmSnapshot, evmRevert, runAsSigner } from '../helpers';
 import { expect } from 'chai';
 
 describe('AnteBoredApeMaxSupplyTest', function () {
   let test: AnteBoredApeMaxSupplyTest;
-  let bayc: IERC721;
+  let bayc: BoredApes;
   let globalSnapshotId: string;
 
   before(async () => {
     globalSnapshotId = await evmSnapshot();
-    bayc = <IERC721>(
+    bayc = <BoredApes>(
       await hre.ethers.getContractAt(
         '@openzeppelin/contracts/token/ERC721/IERC721.sol:IERC721',
         '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'
