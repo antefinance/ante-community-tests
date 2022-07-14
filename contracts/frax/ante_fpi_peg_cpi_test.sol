15cb8e9a69f150caada000a4fbc70f863e8ceab9
Daniel <daniel@crosstechit.com>
2022-07-14 12:23:39 +0100

    improve frax test

--- contracts/frax/ante_fpi_peg_cpi_test.sol
+++ contracts/frax/ante_fpi_peg_cpi_test.sol
@@ -13,11 +13,13 @@ interface IFPIControllerPool {
             uint256,
             bool
         );
+
+    function peg_band_mint_redeem() external view returns (uint256);
 }
 
 /// @title  FPI price remains within expected range of CPI Peg
-/// @notice Ante Test to check that the price of FPI doesn't deviate from the expected range of the CPI Peg Price
-contract AnteFpiPegCpiTest is AnteTest("FPI price remains within the expected range of CPI Peg") {
+/// @notice Ante Test to check that the price of FPI doesn't deviate from the expected range of the CPI Peg Price, but not more than 5%
+contract AnteFpiPegCpiTest is AnteTest("FPI price remains within the expected range of CPI Peg, but not more than 5%") {
     address public constant FPI_CONTROLLER_ADDRESS = 0x2397321b301B80A1C0911d6f9ED4b6033d43cF51;
     address public constant FPI_TOKEN_ADDRESS = 0x5Ca135cB8527d76e932f34B5145575F9d8cbE08E;
 
@@ -31,7 +33,8 @@ contract AnteFpiPegCpiTest is AnteTest("FPI price remains within the expected ra
     /// @return true if the price is within the expected range
     function checkTestPasses() public view override returns (bool) {
         (, , bool withinRange) = fpiController.pegStatusMntRdm();
+        uint256 pegBand = fpiController.peg_band_mint_redeem();
 
-        return withinRange;
+        return withinRange && pegBand <= 50000;
     }
 }
