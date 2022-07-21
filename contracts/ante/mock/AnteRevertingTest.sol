ec5073d93c27b2f5902b373177e1a99823a70e4a
A bit whale-ish <85895672+abitwhaleish@users.noreply.github.com>
2022-07-20 17:27:07 -1000

    add ante avl tests (#96)

---
+++ contracts/ante/mock/AnteRevertingTest.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+// ┏━━━┓━━━━━┏┓━━━━━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┏━┓┃━━━━┏┛┗┓━━━━━━━━┃┏━━┛━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┗━┛┃┏━┓━┗┓┏┛┏━━┓━━━━┃┗━━┓┏┓┏━┓━┏━━┓━┏━┓━┏━━┓┏━━┓
+// ┃┏━┓┃┃┏┓┓━┃┃━┃┏┓┃━━━━┃┏━━┛┣┫┃┏┓┓┗━┓┃━┃┏┓┓┃┏━┛┃┏┓┃
+// ┃┃ ┃┃┃┃┃┃━┃┗┓┃┃━┫━┏┓━┃┃━━━┃┃┃┃┃┃┃┗┛┗┓┃┃┃┃┃┗━┓┃┃━┫
+// ┗┛ ┗┛┗┛┗┛━┗━┛┗━━┛━┗┛━┗┛━━━┗┛┗┛┗┛┗━━━┛┗┛┗┛┗━━┛┗━━┛
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+pragma solidity ^0.7.0;
+
+import "../../libraries/ante-v05-core/AnteTest.sol";
+
+contract AnteRevertingTest is AnteTest("This test reverts in a toggleable manner") {
+    bool public willRevert = false;
+
+    function setWillRevert(bool _willRevert) external {
+        willRevert = _willRevert;
+    }
+
+    function checkTestPasses() public view override returns (bool) {
+        require(!willRevert, "Test reverts");
+
+        return true;
+    }
+}
