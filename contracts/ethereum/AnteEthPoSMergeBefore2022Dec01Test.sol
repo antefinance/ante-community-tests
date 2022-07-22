8c1714f448477a2c271b76f052e5bbbe20401682
A bit whale-ish <85895672+abitwhaleish@users.noreply.github.com>
2022-07-22 09:17:48 -0700

    fix typo

--- contracts/ethereum/AnteEthPoSMergeBefore2022Dec01Test.sol
+++ contracts/ethereum/AnteEthPoSMergeBefore2022Dec01Test.sol
@@ -14,9 +14,10 @@ pragma solidity ^0.7.0;
 import "../libraries/ante-v05-core/AnteTest.sol";
 
 /// @title  Ethereum PoS Merge happens before 2022-12-01 (Beacon Chain turns 2 yrs old)
-/// @notice Ante Test to check if The Merge between Ethereum PoW has happened before
-///         2022-12-01 12:00:23 UTC. This marks the 2-year anniversary of the genesis
-///         block of the Beacon Chain PoS system (see https://beaconscan.com/slot/0).
+/// @notice Ante Test to check if The Merge between Ethereum PoW and PoS has happened
+///         before 2022-12-01 12:00:23 UTC. This marks the 2-year anniversary of the
+///         genesis block of the Beacon Chain PoS system.
+///         (see https://beaconscan.com/slot/0)
 contract AnteEthPoSMergeBefore2022Dec01Test is
     AnteTest("The Merge (Ethereum PoS) happens before 2022-12-01 (Beacon Chain turns 2 yrs old)")
 {
