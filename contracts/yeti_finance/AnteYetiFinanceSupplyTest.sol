6f947477c27a6beb95867f10eb8685b53e7629c3
strobie <0xstrobe@protonmail.com>
2022-08-05 21:54:52 +0000

    remove chainlink cuz we dont use it

--- contracts/yeti_finance/AnteYetiFinanceSupplyTest.sol
+++ contracts/yeti_finance/AnteYetiFinanceSupplyTest.sol
@@ -2,7 +2,6 @@
 
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./FixedPointMathLib.sol";
 import "../AnteTest.sol";
 import "../interfaces/IERC20.sol";
