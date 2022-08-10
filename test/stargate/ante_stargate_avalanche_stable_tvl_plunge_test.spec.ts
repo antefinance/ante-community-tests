494221da0bf1cd51478b90c0923fb81a07dc2e6a
abitwhaleish <abitwhaleish@ante.finance>
2022-08-10 16:12:08 -0700

    update unit tests

--- test/stargate/ante_stargate_avalanche_stable_tvl_plunge_test.spec.ts
+++ test/stargate/ante_stargate_avalanche_stable_tvl_plunge_test.spec.ts
@@ -6,25 +6,44 @@ import {
   AnteStargateAvalancheStableTVLPlungeTest__factory,
 } from '../../typechain';
 
-import { evmSnapshot, evmRevert } from '../helpers';
+import { evmSnapshot, evmRevert, fundSigner, runAsSigner } from '../helpers';
 import { expect } from 'chai';
+import { BigNumber, Contract } from 'ethers';
 
-describe('AnteStargateAvalancheStableTVLPlungeTest', function () {
+describe.only('AnteStargateAvalancheStableTVLPlungeTest', function () {
   let test: AnteStargateAvalancheStableTVLPlungeTest;
+  let usdt: Contract;
+  let usdc: Contract;
 
   let globalSnapshotId: string;
+
+  const usdtAddr = '0x9702230A8Ea53601f5cD2dc00fDBc13d4dF4A8c7';
+  const usdcAddr = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E';
+  const sgUSDTPoolAddr = '0x29e38769f23701A2e4A8Ef0492e19dA4604Be62c';
+  const sgUSDCPoolAddr = '0x1205f31718499dBf1fCa446663B532Ef87481fe1';
+  const targetAddr = '0x1A2B73207C883Ce8E51653d6A9cC8a022740cCA4';
+
+  let startUSDTBalance: BigNumber;
+  let startUSDCBalance: BigNumber;
 
   before(async () => {
     globalSnapshotId = await evmSnapshot();
 
     const [deployer] = waffle.provider.getWallets();
 
+    // Deploy Ante Test
     const factory = (await hre.ethers.getContractFactory(
       'AnteStargateAvalancheStableTVLPlungeTest',
       deployer
     )) as AnteStargateAvalancheStableTVLPlungeTest__factory;
     test = await factory.deploy();
     await test.deployed();
+
+    // Get balances on deploy
+    usdt = await hre.ethers.getContractAt('contracts/interfaces/IERC20.sol:IERC20', usdtAddr, deployer);
+    usdc = await hre.ethers.getContractAt('contracts/interfaces/IERC20.sol:IERC20', usdcAddr, deployer);
+    startUSDTBalance = await usdt.balanceOf(sgUSDTPoolAddr);
+    startUSDCBalance = await usdc.balanceOf(sgUSDCPoolAddr);
   });
 
   after(async () => {
@@ -33,5 +52,37 @@ describe('AnteStargateAvalancheStableTVLPlungeTest', function () {
 
   it('should pass', async () => {
     expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('if USDT/USDC balance drops by 89%, should still pass', async () => {
+    // transfer away 89.9% of pool funds
+    await fundSigner(sgUSDTPoolAddr);
+    await runAsSigner(sgUSDTPoolAddr, async () => {
+      const sgUSDTPoolSigner = await hre.ethers.getSigner(sgUSDTPoolAddr);
+      await usdt.connect(sgUSDTPoolSigner).transfer(targetAddr, startUSDTBalance.mul(9).div(10).sub(1));
+    });
+
+    await fundSigner(sgUSDCPoolAddr);
+    await runAsSigner(sgUSDCPoolAddr, async () => {
+      const sgUSDCPoolSigner = await hre.ethers.getSigner(sgUSDCPoolAddr);
+      await usdc.connect(sgUSDCPoolSigner).transfer(targetAddr, startUSDCBalance.mul(9).div(10).sub(1));
+    });
+
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('if USDT/USDC balance drops by 90%, should fail', async () => {
+    // transfer away enough to get 90% of pool funds
+    await runAsSigner(sgUSDTPoolAddr, async () => {
+      const sgUSDTPoolSigner = await hre.ethers.getSigner(sgUSDTPoolAddr);
+      await usdt.connect(sgUSDTPoolSigner).transfer(targetAddr, 2);
+    });
+
+    await runAsSigner(sgUSDCPoolAddr, async () => {
+      const sgUSDCPoolSigner = await hre.ethers.getSigner(sgUSDCPoolAddr);
+      await usdc.connect(sgUSDCPoolSigner).transfer(targetAddr, 2);
+    });
+
+    expect(await test.checkTestPasses()).to.be.false;
   });
 });
--- test/stargate/ante_stargate_ethereum_stable_tvl_plunge_test.spec.ts
+++ test/stargate/ante_stargate_ethereum_stable_tvl_plunge_test.spec.ts
@@ -6,25 +6,44 @@ import {
   AnteStargateEthereumStableTVLPlungeTest__factory,
 } from '../../typechain';
 
-import { evmSnapshot, evmRevert } from '../helpers';
+import { evmSnapshot, evmRevert, fundSigner, runAsSigner } from '../helpers';
 import { expect } from 'chai';
+import { BigNumber, Contract } from 'ethers';
 
-describe('AnteStargateEthereumStableTVLPlungeTest', function () {
+describe.only('AnteStargateEthereumStableTVLPlungeTest', function () {
   let test: AnteStargateEthereumStableTVLPlungeTest;
+  let usdt: Contract;
+  let usdc: Contract;
 
   let globalSnapshotId: string;
+
+  const usdtAddr = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
+  const usdcAddr = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+  const sgUSDTPoolAddr = '0x38EA452219524Bb87e18dE1C24D3bB59510BD783';
+  const sgUSDCPoolAddr = '0xdf0770dF86a8034b3EFEf0A1Bb3c889B8332FF56';
+  const targetAddr = '0x1A2B73207C883Ce8E51653d6A9cC8a022740cCA4';
+
+  let startUSDTBalance: BigNumber;
+  let startUSDCBalance: BigNumber;
 
   before(async () => {
     globalSnapshotId = await evmSnapshot();
 
     const [deployer] = waffle.provider.getWallets();
 
+    // Deploy Ante Test
     const factory = (await hre.ethers.getContractFactory(
       'AnteStargateEthereumStableTVLPlungeTest',
       deployer
     )) as AnteStargateEthereumStableTVLPlungeTest__factory;
     test = await factory.deploy();
     await test.deployed();
+
+    // Get balances on deploy
+    usdt = await hre.ethers.getContractAt('contracts/interfaces/IERC20.sol:IERC20', usdtAddr, deployer);
+    usdc = await hre.ethers.getContractAt('contracts/interfaces/IERC20.sol:IERC20', usdcAddr, deployer);
+    startUSDTBalance = await usdt.balanceOf(sgUSDTPoolAddr);
+    startUSDCBalance = await usdc.balanceOf(sgUSDCPoolAddr);
   });
 
   after(async () => {
@@ -33,5 +52,37 @@ describe('AnteStargateEthereumStableTVLPlungeTest', function () {
 
   it('should pass', async () => {
     expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('if USDT/USDC balance drops by 89%, should still pass', async () => {
+    // transfer away 89.9% of pool funds
+    await fundSigner(sgUSDTPoolAddr);
+    await runAsSigner(sgUSDTPoolAddr, async () => {
+      const sgUSDTPoolSigner = await hre.ethers.getSigner(sgUSDTPoolAddr);
+      await usdt.connect(sgUSDTPoolSigner).transfer(targetAddr, startUSDTBalance.mul(9).div(10).sub(1));
+    });
+
+    await fundSigner(sgUSDCPoolAddr);
+    await runAsSigner(sgUSDCPoolAddr, async () => {
+      const sgUSDCPoolSigner = await hre.ethers.getSigner(sgUSDCPoolAddr);
+      await usdc.connect(sgUSDCPoolSigner).transfer(targetAddr, startUSDCBalance.mul(9).div(10).sub(1));
+    });
+
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+
+  it('if USDT/USDC balance drops by 90%, should fail', async () => {
+    // transfer away enough to get 90% of pool funds
+    await runAsSigner(sgUSDTPoolAddr, async () => {
+      const sgUSDTPoolSigner = await hre.ethers.getSigner(sgUSDTPoolAddr);
+      await usdt.connect(sgUSDTPoolSigner).transfer(targetAddr, 2);
+    });
+
+    await runAsSigner(sgUSDCPoolAddr, async () => {
+      const sgUSDCPoolSigner = await hre.ethers.getSigner(sgUSDCPoolAddr);
+      await usdc.connect(sgUSDCPoolSigner).transfer(targetAddr, 2);
+    });
+
+    expect(await test.checkTestPasses()).to.be.false;
   });
 });
