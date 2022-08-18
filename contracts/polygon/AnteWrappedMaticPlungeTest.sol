bc801d76b5116cc26d1426399271599b7d9ecbc2
yakitori <abc@abc.com>
2022-08-18 16:34:42 -0400

    Updated address

--- contracts/polygon/AnteWrappedMaticPlungeTest.sol
+++ contracts/polygon/AnteWrappedMaticPlungeTest.sol
@@ -8,9 +8,9 @@ import "../AnteTest.sol";
 /// @notice Ante Test to check that Wrapped Matic does NOT drop below 2M (as of Aug 2022)
 contract AnteWrappedMaticPlungeTest is AnteTest("Wrapped_Matic does NOT drop below 2M") {
     // https://polygonscan.com/address/0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270
-    address public immutable w_maticaddr = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+    address public constant w_maticaddr = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
 
-    // 2022-08-18: Balance is ~285M MATIC, so -99.3% is ~2M 
+    // 2022-08-18: Balance is ~285M MATIC, so -99.3% is ~2M
     uint256 public constant RUG_THRESHOLD = 2 * 1000000 * 1e18;
 
     constructor() {
@@ -18,8 +18,8 @@ contract AnteWrappedMaticPlungeTest is AnteTest("Wrapped_Matic does NOT drop bel
         testedContracts = [w_maticaddr];
     }
 
-    /// @notice test to check balance of wrapped matic 
-    /// @return true if wrapped matic is above 2M matic 
+    /// @notice test to check balance of wrapped matic
+    /// @return true if wrapped matic is above 2M matic
     function checkTestPasses() external view override returns (bool) {
         return w_maticaddr.balance >= RUG_THRESHOLD;
     }
