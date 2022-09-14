a6c5c0ccc88ced410a5b19cae078a94a87e532ac
DeLaLunia <delalunia@ante.finance>
2022-09-14 17:51:43 -0500

    fantom usdc test (#117)

---
+++ test/examples/ante_fantom_usdc_peg_test.spec.ts
@@ -0,0 +1,33 @@
+import hre from 'hardhat';
+const { waffle } = hre;
+
+import { AnteFantomUSDCPegTest, AnteFantomUSDCPegTest__factory } from '../../typechain';
+
+import { evmSnapshot, evmRevert } from '../helpers';
+import { expect } from 'chai';
+
+describe('AnteFantomUSDCPegTest', function () {
+  let test: AnteFantomUSDCPegTest;
+
+  let globalSnapshotId: string;
+
+  before(async () => {
+    globalSnapshotId = await evmSnapshot();
+
+    const [deployer] = waffle.provider.getWallets();
+    const factory = (await hre.ethers.getContractFactory(
+      'AnteFantomUSDCPegTest',
+      deployer
+    )) as AnteFantomUSDCPegTest__factory;
+    test = await factory.deploy('0x04068da6c83afcfa0e13ba15a6696662335d5b75');
+    await test.deployed();
+  });
+
+  after(async () => {
+    await evmRevert(globalSnapshotId);
+  });
+
+  it('should pass', async () => {
+    expect(await test.checkTestPasses()).to.be.true;
+  });
+});
