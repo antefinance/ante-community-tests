a890d507d485d366ad2ce8ced5fcf21cb7422d62
DeLaLunia <delalunia@ante.finance>
2022-09-15 14:23:41 -0600

    arbitrum toy tests added

--- test/examples/ante_arbitrum_usdc_peg_test.spec.ts
+++ test/examples/ante_arbitrum_usdc_peg_test.spec.ts
@@ -2,53 +2,32 @@ import hre from 'hardhat';
 const { waffle } = hre;
 
 import { AnteArbitrumUSDCPegTest, AnteArbitrumUSDCPegTest__factory } from '../../typechain';
-import { Contract, ContractFactory } from 'ethers';
 
 import { evmSnapshot, evmRevert } from '../helpers';
 import { expect } from 'chai';
 
 describe('AnteArbitrumUSDCPegTest', function () {
-  //let test: AnteArbitrumUSDCPegTest;
+  let test: AnteArbitrumUSDCPegTest;
 
   let globalSnapshotId: string;
-  let factory: ContractFactory;
-  let test: Contract;
 
-  it('should return true', async function () {
-    const provider = waffle.provider;
-    console.log(provider);
-    const factory = await hre.ethers.getContractFactory('AnteArbitrumUSDCPegTest');
-    const test = await factory.deploy('0xff970a61a04b1ca14834a43f5de4533ebddb5cc8');
+  before(async () => {
+    globalSnapshotId = await evmSnapshot();
 
-    expect(await test.checkTestPasses()).to.be.true;
+    const [deployer] = waffle.provider.getWallets();
+    const factory = (await hre.ethers.getContractFactory(
+      'AnteArbitrumUSDCPegTest',
+      deployer
+    )) as AnteArbitrumUSDCPegTest__factory;
+    test = await factory.deploy('0xff970a61a04b1ca14834a43f5de4533ebddb5cc8');
+    await test.deployed();
   });
 
-  //before(async () => {
-  //  console.log("INIT");
-  //  //globalSnapshotId = await evmSnapshot();
+  after(async () => {
+    await evmRevert(globalSnapshotId);
+  });
 
-  //  const [deployer] = waffle.provider.getWallets();
-  //  console.log(`Deployer ${deployer.address}`);
-  //  console.log(waffle.provider);
-  //  //const factory = (await hre.ethers.getContractFactory(
-  //  //  'AnteArbitrumUSDCPegTest',
-  //  //  //deployer
-  //  //)) as AnteArbitrumUSDCPegTest__factory;
-  //  factory = await hre.ethers.getContractFactory("AnteArbitrumUSDCPegTest");
-  //  console.log("Factory done");
-  //  test = await factory.deploy('0xff970a61a04b1ca14834a43f5de4533ebddb5cc8');
-  //  await test.deployed();
-  //  console.log("Test deployed");
-  //});
-
-  //after(async () => {
-  //  console.log("Reverting");
-  //  //await evmRevert(globalSnapshotId);
-  //  console.log("Reverted");
-  //});
-
-  //it('should pass', async () => {
-  //  console.log("Checking if test passes");
-  //  expect(await test.checkTestPasses()).to.be.true;
-  //});
+  it('should pass', async () => {
+    expect(await test.checkTestPasses()).to.be.true;
+  });
 });
