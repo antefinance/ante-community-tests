f09994018d6c5693234ebc167bcb585dc060cc63
0xTestooor <0xTestooor@proton.me>
2022-10-07 09:52:24 -0700

    more accurate naming

--- contracts/defigirls/AnteDeFiGirlDAODumpTest.sol
+++ contracts/defigirls/AnteDeFiGirlDAODumpTest.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.0;
 import {AnteTest} from "../AnteTest.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
-/// @title Checks that defigirldao.eth doesn't sell any DeFi Girls before 2023-09-01
+/// @title Checks that defigirldao.eth doesn't hold fewer than 780 DeFi Girls before 2023-09-01
 /// @author 0xTestooor
 /// @notice Ante Test to check that the total number of DeFi Girls held by
 /// defigirldao.eth doesn't decrease before 2023-09-01 (commitment made via
@@ -15,7 +15,9 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 /// the open market; note that no NFT may be sold from this wallet until
 /// September 2023 at the earliest, at a max rate of 2 NFTs/Day, and with the
 /// majority consensus of all holders."
-contract AnteDeFiGirlDAODumpTest is AnteTest("DeFiGirlDAO.eth doesn't dump before Sept 2023") {
+/// The test checks the number of DeFi Girls held rather than specific
+/// ownership but is a reasonable approximation
+contract AnteDeFiGirlDAODumpTest is AnteTest("DeFiGirlDAO.eth holds >= 780 DeFi Girls until Sept 2023") {
     // https://etherscan.io/address/0x754bbb703EEada12A6988c0e548306299A263a08
     address public constant DEFIGIRLDAO = 0x754bbb703EEada12A6988c0e548306299A263a08;
     // https://etherscan.io/address/0x3B14d194c8CF46402beB9820dc218A15e7B0A38f
@@ -43,8 +45,5 @@ contract AnteDeFiGirlDAODumpTest is AnteTest("DeFiGirlDAO.eth doesn't dump befor
             // otherwise, return true if balance > threshold
             return (DFGIRL.balanceOf(DEFIGIRLDAO) >= thresholdBalance);
         }
-        // technically this means defigirldao.eth could buy/sell DeFi Girls as
-        // long as the total number owned remains at or above 780, but this is
-        // probably a reasonable approximation
     }
 }
