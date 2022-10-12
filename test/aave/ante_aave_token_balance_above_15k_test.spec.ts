c656317408301747bcbdc0ce62a764cdd40471f0
DeLaLunia <delalunia@ante.finance>
2022-10-12 17:06:03 -0600

    Ante test name and comments cleanup

--- test/aave/ante_aave_token_balance_above_15k_test.spec.ts
+++ test/aave/ante_aave_token_balance_above_15k_test.spec.ts
@@ -1,7 +1,6 @@
 import hre from 'hardhat';
 const { waffle } = hre;
 
-// TODO add the typechain files for YourAnteTest, YourAnteTest__factory
 // Note: If you are using an IDE it may warn you that these files are missing.
 // This is expected and OK, they will be generated when you run the test command!
 import { AnteAaveTokenBalanceAbove15K, AnteAaveTokenBalanceAbove15K__factory } from '../../typechain';
@@ -10,17 +9,15 @@ import { evmSnapshot, evmRevert, fundSigner, runAsSigner } from '../helpers';
 import { expect } from 'chai';
 import { BigNumber, Contract } from 'ethers';
 
-// TODO replace 'YourAnteTest'
 describe('AnteAaveTokenBalanceAbove15K', function () {
-  let test: AnteAaveTokenBalanceAbove15K; // TODO replace
+  let test: AnteAaveTokenBalanceAbove15K;
   let token: Contract;
 
   let globalSnapshotId: string;
 
-  // TODO FILL IN WITH TEST PARAMETERS
-  const tokenAddr = '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9'; // TODO fill in
-  const holderAddr = '0x25F2226B597E8F9514B3F68F00f494cF4f286491'; // TODO fill in
-  const failThreshold = 15000; // TODO fill this in without decimal places (e.g. for 100 USDC, 100 instead of 100000000)
+  const tokenAddr = '0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9';
+  const holderAddr = '0x25F2226B597E8F9514B3F68F00f494cF4f286491';
+  const failThreshold = 15000;
 
   const sifuAddr = '0x5DD596C901987A2b28C38A9C1DfBf86fFFc15d77'; // used as throwaway address to transfer tokens
 
@@ -34,9 +31,9 @@ describe('AnteAaveTokenBalanceAbove15K', function () {
 
     // Deploy Ante Test
     const factory = (await hre.ethers.getContractFactory(
-      'AnteAaveTokenBalanceAbove15K', // TODO replace this
+      'AnteAaveTokenBalanceAbove15K',
       deployer
-    )) as AnteAaveTokenBalanceAbove15K__factory; // TODO replace this
+    )) as AnteAaveTokenBalanceAbove15K__factory;
     test = await factory.deploy();
     await test.deployed();
 
@@ -57,7 +54,7 @@ describe('AnteAaveTokenBalanceAbove15K', function () {
     expect(await test.checkTestPasses()).to.be.true;
   });
 
-  it('if token balance drops under [AMOUNT], should fail', async () => {
+  it('if token balance drops under 15k, should fail', async () => {
     // transfer away enough to meet failure condition
     await fundSigner(holderAddr);
     await runAsSigner(holderAddr, async () => {
