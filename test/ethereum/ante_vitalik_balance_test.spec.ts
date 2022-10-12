f8d8e638751e5ae5573d9fd6012e3522b5f082f1
DeLaLunia <delalunia@ante.finance>
2022-10-12 17:27:39 -0600

    comments and name cleanup

--- test/ethereum/ante_vitalik_balance_test.spec.ts
+++ test/ethereum/ante_vitalik_balance_test.spec.ts
@@ -1,26 +1,21 @@
 import hre from 'hardhat';
 const { waffle } = hre;
 
-// TODO add the typechain files for YourAnteTest, YourAnteTest__factory
-// Note: If you are using an IDE it may warn you that these files are missing.
-// This is expected and OK, they will be generated when you run the test command!
-import { VitalikBalance, VitalikBalance__factory } from '../typechain';
+import { AnteVitalikBalanceTest, AnteVitalikBalanceTest__factory } from '../../typechain';
 
-import { evmSnapshot, evmRevert, fundSigner, runAsSigner } from './helpers';
+import { evmSnapshot, evmRevert, fundSigner, runAsSigner } from '../helpers';
 import { expect } from 'chai';
 import { BigNumber, Contract } from 'ethers';
 
-// TODO replace 'YourAnteTest'
-describe('VitalikBalance', function () {
-  let test: VitalikBalance; // TODO replace
+describe('AnteVitalikBalanceTest', function () {
+  let test: AnteVitalikBalanceTest;
   let token: Contract;
 
   let globalSnapshotId: string;
 
-  // TODO FILL IN WITH TEST PARAMETERS
-  const tokenAddr = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // TODO fill in
-  const holderAddr = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'; // TODO fill in
-  const failThreshold = 1; // TODO fill this in without decimal places (e.g. for 100 USDC, 100 instead of 100000000)
+  const tokenAddr = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+  const holderAddr = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
+  const failThreshold = 1;
 
   const sifuAddr = '0x5DD596C901987A2b28C38A9C1DfBf86fFFc15d77'; // used as throwaway address to transfer tokens
 
@@ -34,9 +29,9 @@ describe('VitalikBalance', function () {
 
     // Deploy Ante Test
     const factory = (await hre.ethers.getContractFactory(
-      'VitalikBalance', // TODO replace this
+      'AnteVitalikBalanceTest',
       deployer
-    )) as VitalikBalance__factory; // TODO replace this
+    )) as AnteVitalikBalanceTest__factory;
     test = await factory.deploy();
     await test.deployed();
 
@@ -57,7 +52,7 @@ describe('VitalikBalance', function () {
     expect(await test.checkTestPasses()).to.be.true;
   });
 
-  it('if token balance drops under [AMOUNT], should fail', async () => {
+  it('if token balance drops under 1, should fail', async () => {
     // transfer away enough to meet failure condition
     await fundSigner(holderAddr);
     await runAsSigner(holderAddr, async () => {
