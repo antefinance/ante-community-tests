729c1a0f8eeec9176dc3380c490af681116e4abd
DeLaLunia <delalunia@ante.finance>
2022-10-21 00:37:42 -0500

    dai to wbtc in across optimistic bridge (#133)
    * dai to wbtc in across optimistic bridge
    
    * test descriptions clarification and comment fixes
    
    * typo fix

--- contracts/across/AnteAcrossOptimisticBridgeTest.sol
+++ contracts/across/AnteAcrossOptimisticBridgeTest.sol
@@ -6,33 +6,32 @@ import "../AnteTest.sol";
 import "@openzeppelin-contracts-old/contracts/token/ERC20/IERC20.sol";
 
 /// @title AcrossBridgeTest
-/// @notice Ante Test to check if Across Optimistic "rugs" 70% of its value (as of test deployment)
+/// @notice Ante Test to check if Across Optimistic "rugs" 70% of its top 3 tokens (as of test deployment)
 
-contract AnteAcrossOptimisticBridgeTest is AnteTest("Across Bridge does not RUG its 70 % of its value") {
+contract AnteAcrossOptimisticBridgeTest is AnteTest("Across Bridge does not rug 70% of its top 3 tokens") {
     // Contracts
     // https://docs.across.to/v2/developers/contract-addresses/mainnet-chain-id-1
-
-    // HubPool Address: The  main contract Holds Assets
+    // HubPool Address: The main contract that holds assets
     address public constant hubPoolAddr = 0xc186fA914353c44b2E33eBE05f21846F1048bEda;
 
     // Pool Assetss
 
-    // Lets 3 Major Assets:  WETH USDC DAI
+    // Lets use 3 Major Assets: WETH WBTC USDC
     // WETH:  https://etherscan.io/address/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
     address public constant wethAddr = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    // Dai:  https://etherscan.io/address/0x6B175474E89094C44Da98b954EedeAC495271d0F
-    address public constant daiAddr = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    // WBTC:  https://etherscan.io/address/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599
+    address public constant wbtcAddr = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
     // USDC: https://etherscan.io/address/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
     address public constant usdcAddr = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
     uint256 public constant THRESHOLD = 30;
 
     IERC20 private constant WETH = IERC20(wethAddr);
-    IERC20 private constant DAI = IERC20(daiAddr);
+    IERC20 private constant WBTC = IERC20(wbtcAddr);
     IERC20 private constant USDC = IERC20(usdcAddr);
 
     uint256 public immutable wethThreshold;
-    uint256 public immutable daiThreshold;
+    uint256 public immutable wbtcThreshold;
     uint256 public immutable usdcThreshold;
 
     constructor() {
@@ -40,7 +39,7 @@ contract AnteAcrossOptimisticBridgeTest is AnteTest("Across Bridge does not RUG 
         testedContracts = [hubPoolAddr];
 
         wethThreshold = (WETH.balanceOf(hubPoolAddr) * THRESHOLD) / 100;
-        daiThreshold = (DAI.balanceOf(hubPoolAddr) * THRESHOLD) / 100;
+        wbtcThreshold = (WBTC.balanceOf(hubPoolAddr) * THRESHOLD) / 100;
         usdcThreshold = (USDC.balanceOf(hubPoolAddr) * THRESHOLD) / 100;
     }
 
@@ -49,7 +48,7 @@ contract AnteAcrossOptimisticBridgeTest is AnteTest("Across Bridge does not RUG 
     function checkTestPasses() external view override returns (bool) {
         return
             wethThreshold < WETH.balanceOf(hubPoolAddr) &&
-            daiThreshold < DAI.balanceOf(hubPoolAddr) &&
+            wbtcThreshold < WBTC.balanceOf(hubPoolAddr) &&
             usdcThreshold < USDC.balanceOf(hubPoolAddr);
     }
 }
