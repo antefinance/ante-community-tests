0c3367109bc042f5cbb255de719b4a13825a6251
abitwhaleish <abitwhaleish@ante.finance>
2022-10-25 13:54:16 -0700

    add author natspec

--- contracts/anoncats/AnteAnoncatOverpopulationTest.sol
+++ contracts/anoncats/AnteAnoncatOverpopulationTest.sol
@@ -6,6 +6,7 @@ import {AnteTest} from "../AnteTest.sol";
 import {IERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 /// @title Checks that total supply of Anoncats does not exceed 100
+/// @author abitwhaleish.eth (0x1A2B73207C883Ce8E51653d6A9cC8a022740cCA4)
 /// @notice Ante Test to check if the total supply of Anoncats exceeds 100
 contract AnteAnoncatOverpopulationTest is AnteTest("No more than 100 Anoncats exist") {
     // https://etherscan.io/address/0xe7141C205a7a74241551dAF007537A041867e0B0
