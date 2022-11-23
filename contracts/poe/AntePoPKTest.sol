bba67a8947effe6181a8c33d5a880f5d6e464a4f
0xHegemon <0xhegemon@gmail.com>
2022-11-23 11:47:56 -0500

    prepare for Goerli deploy

--- contracts/poe/AntePoPKTest.sol
+++ contracts/poe/AntePoPKTest.sol
@@ -1,18 +1,22 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
+// ┏━━━┓━━━━━┏┓━━━━━━━━━┏━━━┓━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┏━┓┃━━━━┏┛┗┓━━━━━━━━┃┏━━┛━━━━━━━━━━━━━━━━━━━━━━━
+// ┃┗━┛┃┏━┓━┗┓┏┛┏━━┓━━━━┃┗━━┓┏┓┏━┓━┏━━┓━┏━┓━┏━━┓┏━━┓
+// ┃┏━┓┃┃┏┓┓━┃┃━┃┏┓┃━━━━┃┏━━┛┣┫┃┏┓┓┗━┓┃━┃┏┓┓┃┏━┛┃┏┓┃
+// ┃┃ ┃┃┃┃┃┃━┃┗┓┃┃━┫━┏┓━┃┃━━━┃┃┃┃┃┃┃┗┛┗┓┃┃┃┃┃┗━┓┃┃━┫
+// ┗┛ ┗┛┗┛┗┛━┗━┛┗━━┛━┗┛━┗┛━━━┗┛┗┛┗┛┗━━━┛┗┛┗┛┗━━┛┗━━┛
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+// ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
 pragma solidity ^0.8.0;
 
 import {AnteTest} from "../AnteTest.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-// ==INSTRUCTIONS==
-// TODO 1. Rename the contract and file in the form Ante[DescriptiveNameInCamelCase]Test
-// TODO 2. Update checkTestPasses with pass/fail logic
-
-/// @title This should succinctly explain what the Ante Test checks
-/// @notice Ante Test to check _____
+/// @title (Demo) Nobody else should know the private key to this public ETH address
+/// @notice Ante Test to check that the signature to the message "AntePoPKTest Demo" does not correspond to the testAddress
 contract AntePoPKTest is AnteTest("Nobody else knows the private key of this public address") {
-    // Here is where any variables you might use can be declared, e.g. token addresses
     // A non-custom string that is frontrunnable
     string message = "AntePoPKTest Demo";
     bytes32 message_hash = ECDSA.toEthSignedMessageHash(keccak256(abi.encodePacked(message)));
@@ -21,17 +25,14 @@ contract AntePoPKTest is AnteTest("Nobody else knows the private key of this pub
     address public testAddress = 0x66c777464c62F125760f80254257ed8DFccB2921;
 
     constructor() {
-        // TODO replace "Protocol" with target protocol/wallet/etc.
-        protocolName = "Protocol";
-
-        // TODO replace 0x0 with any contracts being tested
+        protocolName = "ProofOfExploit";
         testedContracts = [address(0)];
     }
 
-    /// @notice test to check if $[TOKEN] balance in [TARGET] is >= [THRESHOLD]
-    /// @return true if $[TOKEN] balance in [TARGET] is >= [THRESHOLD]
+    /// @notice Check that the signature submitted does not correspond to the testAddress
+    /// @return true if ECDSA.recover(message_hash, signature) != testAddress
     function checkTestPasses() public view override returns (bool) {
-        // Here is where the test logic lives!
+        // The signature must have length 65 in order to be valid
         if (signature.length == 65) {
             return ECDSA.recover(message_hash, signature) != testAddress;
         }
