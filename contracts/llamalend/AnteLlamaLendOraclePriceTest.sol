b018a74efadce286e6c5ec635c64126cc1c58028
abitwhaleish <abitwhaleish@ante.finance>
2022-12-09 18:21:13 -0800

    update failure price

--- contracts/llamalend/AnteLlamaLendOraclePriceTest.sol
+++ contracts/llamalend/AnteLlamaLendOraclePriceTest.sol
@@ -9,16 +9,17 @@ pragma solidity ^0.8.0;
 
 import {AnteTest} from "../AnteTest.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {LendingPool} from "./llamalend-contracts/LendingPool.sol";
 
-/// @title Checks that the LlamaLend TubbyLoan oracle never
-///        returns a Tubby Cats price greater than 1 ETH.
+interface ILendingPool {
+    function oracle() external view returns (address);
+}
+
+/// @title Checks that the LlamaLend oracle used by TubbyLoan never
+///        returns a Tubby Cats floor price greater than 0.3 ETH.
 /// @author 0x1A2B73207C883Ce8E51653d6A9cC8a022740cCA4 (abitwhaleish.eth)
 /// @notice Ante Test to check the LlamaLend oracle used by the TubbyLoan
-///         pool never returns a Tubby Cats price greater than 1 ETH
-contract AnteLlamaLendOraclePriceTest is
-    AnteTest("LlamaLend oracle for TubbyLoan never returns Tubby Cats price > 1 ETH")
-{
+///         pool never returns a Tubby Cats price greater than 0.3 ETH
+contract AnteLlamaLendOraclePriceTest is AnteTest("LlamaLend oracle never returns Tubby Cats price > 0.3 ETH") {
     struct Message {
         bytes32 hash;
         uint8 v;
@@ -27,13 +28,13 @@ contract AnteLlamaLendOraclePriceTest is
     }
 
     // https://etherscan.io/address/0x34d0A4B1265619F3cAa97608B621a17531c5626f
-    LendingPool public constant LLAMALEND_TUBBYLOAN_POOL = LendingPool(0x34d0A4B1265619F3cAa97608B621a17531c5626f);
+    ILendingPool public constant LLAMALEND_TUBBYLOAN_POOL = ILendingPool(0x34d0A4B1265619F3cAa97608B621a17531c5626f);
 
     // https://etherscan.io/address/0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD
     address public constant TUBBY_CATS = 0xCa7cA7BcC765F77339bE2d648BA53ce9c8a262bD;
 
     // The price that we don't think the oracle should return above
-    uint256 public constant FAILURE_PRICE = 1e18; // 1 ETH
+    uint256 public constant FAILURE_PRICE = 3e17; // 0.3 ETH
 
     // Message storage variable
     Message public message;
@@ -69,9 +70,9 @@ contract AnteLlamaLendOraclePriceTest is
      * ================ USER INTERFACE ================= *
      *****************************************************/
 
-    /// @notice Sets the message parameters to check for a valid signature.
-    ///         As of 2022-12-07, the following API endpoint can be used to
-    ///         get the latest signed message from the oracle:
+    /// @notice Sets the message parameters to check for a valid signature. As
+    ///         of 2022-12-07, the following API endpoint can be used to get
+    ///         the latest signed message from the Tubby Cats price oracle:
     ///         https://oracle.llamalend.com/quote/1/0xca7ca7bcc765f77339be2d648ba53ce9c8a262bd
     /// @param price floor price of collection
     /// @param deadline deadline of floor price validity
@@ -91,7 +92,7 @@ contract AnteLlamaLendOraclePriceTest is
         // so we can generate the correct message body to match
         require(price > FAILURE_PRICE, "Price not above failing level!");
 
-        // Store the hashed message body and signature
+        // Store the hashed message body and signature to check
         message = Message(
             keccak256(
                 abi.encodePacked("\x19Ethereum Signed Message:\n111", price, deadline, block.chainid, TUBBY_CATS)
