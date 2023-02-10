54d5125e777ee73aa3c53cc591db40baf5261026
DeLaLunia <delalunia@ante.finance>
2023-02-09 20:30:32 -0700

    Busd test renamed

--- contracts/templates/AnteTokenSupplyThresholdTemplate.sol
+++ contracts/templates/AnteTokenSupplyThresholdTemplate.sol
@@ -8,23 +8,29 @@ import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 /// @title Supply threshold test for token [TOKEN]
 /// @author [AUTHOR]
 /// @notice Ante test to check that the supply of [TOKEN] does not
-///      exceed the threshold [THRESHOLD]
+///         exceed the threshold [THRESHOLD]
 contract AnteSupplyThresholdTestTemplate is AnteTest("[TOKEN] supply doesn't exceed [THRESHOLD]") {
-    address public immutable tokenAddress;
-    uint256 public constant THRESHOLD_SUPPLY = [THRESHOLD];
-    IERC20 public token;
+    // TODO update token address and block explorer link
+    // https://etherscan.io/address/0x6123B0049F904d730dB3C36a31167D9d4121fA6B
+    IERC20Metadata public constant TOKEN = IERC20Metadata(0x6123B0049F904d730dB3C36a31167D9d4121fA6B);
+
+    // Will be set to desired token balance threshold
+    uint256 public immutable thresholdSupply;
 
     constructor() {
-        protocolName = [TOKEN];
-        testedContracts = [[TOKEN_ADDRESS]];
 
-        tokenAddress = [TOKEN_ADDRESS];
-        token = IERC20(tokenAddress);
+        // TODO replace (1000 * 1000) with desired threshold
+        thresholdSupply = (1000 * 1000) * (10**TOKEN.decimals());
+
+        // TODO replace "Protocol" with target protocol/wallet/etc.
+        protocolName = "Protocol";
+
+        testedContracts = [address(TOKEN)];
     }
 
     /// @notice Test to check token supply against threshold
     /// @return [TOKEN] supply is less than [THRESHOLD]
     function checkTestPasses() external view override returns (bool) {
-        return (token.totalSupply() <= THRESHOLD_SUPPLY);
-    };
-};
+        return (TOKEN.totalSupply() <= thresholdSupply);
+    }
+}
