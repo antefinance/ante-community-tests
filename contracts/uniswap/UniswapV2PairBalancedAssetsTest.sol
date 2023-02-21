28d7c6973316ef4db766fff404bfdd00591fe30b
kokosro <kokos@kokos.ro>
2023-02-21 06:33:38 +0200

    using feed registry contract to compile token value in usd

--- contracts/uniswap/UniswapV2PairBalancedAssetsTest.sol
+++ contracts/uniswap/UniswapV2PairBalancedAssetsTest.sol
@@ -5,10 +5,9 @@ pragma solidity ^0.8.0;
 import {AnteTest} from "../libraries/ante-v06-core/AnteTest.sol";
 import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-interface IChainlinkAggregator {
-    function latestAnswer() external view returns (int256);
-    function decimals() external view returns (uint8);
-}
+import { FeedRegistryInterface } from "@chainlink/contracts/src/v0.8/interfaces/FeedRegistryInterface.sol";
+import { AggregatorV2V3Interface } from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV2V3Interface.sol";
+import { Denominations } from "@chainlink/contracts/src/v0.8/Denominations.sol";
 
 interface IUniswapV2Pair is IERC20Metadata {
   function token0() external view returns (address);
@@ -25,19 +24,19 @@ interface IUniswapV2Factory {
 /// @title Check if Uniswap V2 Pair has balanced assets
 /// @notice Ante Test to check if Uniswap V2 Pair has balanced assets
 contract UniswapV2PairBalancedAssetsTest is AnteTest("Uniswap V2 Pair Balanced Assets Test") {
-
+  address public constant FEED_REGISTRY = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf;
+  address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
   address public factoryAddress;
   address public pairAddress;
 
   uint256 public acceptedDeviation;
 
-  mapping (address => address) public chainlinkFeedUSD;
-
   constructor(uint256 _acceptedDeviation, address _factoryAddress, address _initialTokenA, address _initialTokenB) {
     require(_factoryAddress != address(0), "UniswapV2PairBalancedAssetsTest: factory address cannot be 0x0");
     require(_acceptedDeviation > 0 &&
             _acceptedDeviation < 100, "UniswapV2PairBalancedAssetsTest: invalid accepted deviation");
-    
+    require(hasFeed(_initialTokenA), "UniswapV2PairBalancedAssetsTest: token A not supported");
+    require(hasFeed(_initialTokenB), "UniswapV2PairBalancedAssetsTest: token B not supported");
     factoryAddress = _factoryAddress;
 
     // Set initial pair address
@@ -45,58 +44,6 @@ contract UniswapV2PairBalancedAssetsTest is AnteTest("Uniswap V2 Pair Balanced A
     require(pairAddress != address(0), "UniswapV2PairBalancedAssetsTest: pair address cannot be 0x0");
     
     protocolName = "Uniswap V2";
-    
-
-    // CHAINLINK FEED ETH / USD for token WETH
-    chainlinkFeedUSD[0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2] = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
-
-    // CHAINLINK FEED COMP / USD for token COMP
-    chainlinkFeedUSD[0xc00e94Cb662C3520282E6f5717214004A7f26888] = 0xdbd020CAeF83eFd542f4De03e3cF0C28A4428bd5;
-
-    // CHAINLINK FEED KNC / USD for token KNC
-    chainlinkFeedUSD[0xdd974D5C2e2928deA5F71b9825b8b646686BD200] = 0xf8fF43E991A81e6eC886a3D281A2C6cC19aE70Fc;
-
-    // CHAINLINK FEED LINK / USD for token LINK
-    chainlinkFeedUSD[0x514910771AF9Ca656af840dff83E8264EcF986CA] = 0x2c1d072e956AFFC0D435Cb7AC38EF18d24d9127c;
-    
-    // CHAINLINK FEED MANA / USD for token MANA
-    chainlinkFeedUSD[0x0F5D2fB29fb7d3CFeE444a200298f468908cC942] = 0x56a4857acbcfe3a66965c251628B1c9f1c408C19;
-
-    // CHAINLINK FEED USDP / USD for token USDP
-    chainlinkFeedUSD[0x8E870D67F660D95d5be530380D0eC0bd388289E1] = 0x09023c0DA49Aaf8fc3fA3ADF34C6A7016D38D5e3;
-
-    // CHAINLINK FEED TUSD / USD for token TUSD
-    chainlinkFeedUSD[0x0000000000085d4780B73119b644AE5ecd22b376] = 0xec746eCF986E2927Abd291a2A1716c940100f8Ba;
-
-    // CHAINLINK FEED USDC / USD for token USDC
-    chainlinkFeedUSD[0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48] = 0x8fFfFfd4AfB6115b954Bd326cbe7B4BA576818f6;
-
-    // CHAINLINK FEED USDT / USD for token USDT
-    chainlinkFeedUSD[0xdAC17F958D2ee523a2206206994597C13D831ec7] = 0x3E7d1eAB13ad0104d2750B8863b489D65364e32D;
-
-    // CHAINLINK FEED ZRX / USD for token ZRX
-    chainlinkFeedUSD[0xE41d2489571d322189246DaFA5ebDe1F4699F498] = 0x2885d15b8Af22648b98B122b22FDF4D2a56c6023;
-
-    // CHAINLINK FEED BAL / USD for token BAL
-    chainlinkFeedUSD[0xba100000625a3754423978a60c9317c58a424e3D] = 0xdF2917806E30300537aEB49A7663062F4d1F2b5F;
-
-    // CHAINLINK FEED YFI / USD for token YFI
-    chainlinkFeedUSD[0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e] = 0xA027702dbb89fbd58938e4324ac03B58d812b0E1;
-
-    // CHAINLINK FEED GUSD / USD for token GUSD
-    chainlinkFeedUSD[0x056Fd409E1d7A124BD7017459dFEa2F387b6d5Cd] = 0xa89f5d2365ce98B3cD68012b6f503ab1416245Fc;
-
-    // CHAINLINK FEED UNI / USD for token UNI
-    chainlinkFeedUSD[0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984] = 0x553303d460EE0afB37EdFf9bE42922D8FF63220e;
-
-    // CHAINLINK FEED BTC / USD for token WBTC
-    chainlinkFeedUSD[0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599] = 0xF4030086522a5bEEa4988F8cA5B36dbC97BeE88c;
-
-    // CHAINLINK FEED AAVE / USD for token AAVE
-    chainlinkFeedUSD[0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9] = 0x547a514d5e3769680Ce22B2361c10Ea13619e8a9;
-
-    // CHAINLINK FEED MATIC / USD for token MATIC
-    chainlinkFeedUSD[0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0] = 0x7bAC85A8a13A4BcD8abb3eB7d6b4d632c5a57676;
 
     testedContracts = [
       factoryAddress
@@ -126,27 +73,90 @@ contract UniswapV2PairBalancedAssetsTest is AnteTest("Uniswap V2 Pair Balanced A
 
   }
 
+  function tryGetEthAggregator(address _token) public view returns (AggregatorV2V3Interface) {
+    try FeedRegistryInterface(FEED_REGISTRY).getFeed(_token, Denominations.ETH) returns (AggregatorV2V3Interface feed) {
+      return feed;
+    } catch {
+      return AggregatorV2V3Interface(address(0));
+    }
+  }
+
+  function tryGetBtcAggregator(address _token) public view returns (AggregatorV2V3Interface) {
+    try FeedRegistryInterface(FEED_REGISTRY).getFeed(_token, Denominations.BTC) returns (AggregatorV2V3Interface feed) {
+      return feed;
+    } catch {
+      return AggregatorV2V3Interface(address(0));
+    }
+  }
+
+  function tryGetUsdAggregator(address _token) public view returns (AggregatorV2V3Interface) {
+    try FeedRegistryInterface(FEED_REGISTRY).getFeed(_token, Denominations.USD) returns (AggregatorV2V3Interface feed) {
+      return feed;
+    } catch {
+      return AggregatorV2V3Interface(address(0));
+    }
+  }
+
+  function hasEthFeed(address _token) public view returns (bool){
+    return address(tryGetEthAggregator(_token)) != address(0);
+  }
+
+  function hasBtcFeed(address _token) public view returns (bool){
+    return address(tryGetBtcAggregator(_token)) != address(0);
+  }
+
+  function hasUsdFeed(address _token) public view returns (bool){
+    return address(tryGetUsdAggregator(_token)) != address(0);
+  }
+
+  function hasFeed(address _token) public view returns (bool) {
+    if(_token == WETH) {
+      return true;
+    }
+    return hasEthFeed(_token) || hasBtcFeed(_token) || hasUsdFeed(_token);
+  }
+  function getUsdValue(address _token, uint256 _amount) public view returns (uint256) {
+    uint256 tokenDecimals = IERC20Metadata(_token).decimals();
+    uint256 tokenPrice;
+    if(_token == WETH) {
+      tokenPrice = uint256(FeedRegistryInterface(FEED_REGISTRY).latestAnswer(Denominations.ETH, Denominations.USD));
+    } else {
+      if(hasUsdFeed(_token)){
+        tokenPrice = uint256(FeedRegistryInterface(FEED_REGISTRY).latestAnswer(_token, Denominations.USD));
+      } else if(hasEthFeed(_token)) {
+        uint256 tokenPriceEth = uint256(FeedRegistryInterface(FEED_REGISTRY)
+                                        .latestAnswer(_token, Denominations.ETH));
+        uint256 ethPrice = uint256(FeedRegistryInterface(FEED_REGISTRY)
+                                        .latestAnswer(Denominations.ETH, Denominations.USD));
+        tokenPrice = ethPrice * tokenPriceEth / 10 ** 8;
+      } else if(hasBtcFeed(_token)) {
+        uint256 tokenPriceBtc = uint256(FeedRegistryInterface(FEED_REGISTRY)
+                                        .latestAnswer(_token, Denominations.BTC));
+        uint256 btcPrice = uint256(FeedRegistryInterface(FEED_REGISTRY)
+                                        .latestAnswer(Denominations.BTC, Denominations.USD));
+        tokenPrice = btcPrice * tokenPriceBtc / 10 ** 8;
+      }
+    }
+    return _amount * tokenPrice / (10 ** tokenDecimals);
+    
+  }
+
   function getPairReservesValues() public view returns (uint256, uint256) {
     address token0 = IUniswapV2Pair(pairAddress).token0();
     address token1 = IUniswapV2Pair(pairAddress).token1();
     (uint256 token0Balance, uint256 token1Balance,) = IUniswapV2Pair(pairAddress).getReserves();
 
-    uint256 token0Decimals = IERC20Metadata(token0).decimals();
-    uint256 token1Decimals = IERC20Metadata(token1).decimals();
-
-    uint256 token0Price = uint256(IChainlinkAggregator(chainlinkFeedUSD[token0]).latestAnswer());
-    uint256 token1Price = uint256(IChainlinkAggregator(chainlinkFeedUSD[token1]).latestAnswer());
-
-    uint256 token0Value = token0Balance * token0Price / (10 ** token0Decimals);
-    uint256 token1Value = token1Balance * token1Price / (10 ** token1Decimals);
+    uint256 token0Value = getUsdValue(token0, token0Balance);
+    uint256 token1Value = getUsdValue(token1, token1Balance);
 
     return (token0Value, token1Value);
   }
 
   function _setState(bytes memory _state) internal override {
     (address tokenA, address tokenB) = abi.decode(_state, (address, address));
-    require(chainlinkFeedUSD[tokenA] != address(0), "Token A not supported");
-    require(chainlinkFeedUSD[tokenB] != address(0), "Token B not supported");
+    require(hasFeed(tokenA), "UniswapV2PairBalancedAssetsTest: token A not supported");
+    require(hasFeed(tokenB), "UniswapV2PairBalancedAssetsTest: token B not supported");
+
     address _pair = IUniswapV2Factory(factoryAddress).getPair(tokenA, tokenB);
     require(_pair != address(0), "Pair does not exist");
     pairAddress = _pair;
--- test/uniswap/UniswapV2PairBalanceAssetsTest.spec.ts
+++ test/uniswap/UniswapV2PairBalanceAssetsTest.spec.ts
@@ -12,11 +12,12 @@ describe('UniswapV2PairBalancedAssetsTest', function () {
   let globalSnapshotId: string;
   let acceptedDeviation = 1;
   let factoryAddress = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'; // UniswapV2 factory
-  let initialTokenA = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // WETH
-  let initialTokenB = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'; // USDC
-
-  let stateChangeTokenA = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'; // WETH
-  let stateChangeTokenB = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'; // WBTC
+  const tokens = {
+    WETH: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+    USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
+    WBTC: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
+    BAT: '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
+  };
 
   before(async () => {
     globalSnapshotId = await evmSnapshot();
@@ -26,7 +27,7 @@ describe('UniswapV2PairBalancedAssetsTest', function () {
       'UniswapV2PairBalancedAssetsTest',
       deployer
     )) as UniswapV2PairBalancedAssetsTest__factory;
-    test = await factory.deploy(acceptedDeviation, factoryAddress, initialTokenA, initialTokenB);
+    test = await factory.deploy(acceptedDeviation, factoryAddress, tokens.WETH, tokens.USDC, { gasLimit: 8000000 });
     await test.deployed();
   });
 
@@ -44,21 +45,20 @@ describe('UniswapV2PairBalancedAssetsTest', function () {
     Difference: ${diffPercentage}%
     `);
     */
+
+    // test with WETH and USDC that have a direct USD price feed
     expect(await test.checkTestPasses()).to.be.true;
-    const stateAbiEncoded = ethers.utils.defaultAbiCoder.encode(
-      ['address', 'address'],
-      [stateChangeTokenA, stateChangeTokenB]
-    );
+
+    // test with WETH and BAT that have a USD price feed via ETH
+    let stateAbiEncoded = ethers.utils.defaultAbiCoder.encode(['address', 'address'], [tokens.WETH, tokens.BAT]);
     await test.setStateAndCheckTestPasses(stateAbiEncoded);
-    /*
-    const [value0After, value1After] = await test.getPairReservesValues();
-    const diffPercentageAfter = value0After.sub(value1After).mul(100).div(value0After).abs();
-    console.log(`
-    Reserve0: ${value0After}
-    Reserve1: ${value1After}
-    Difference: ${diffPercentageAfter}%
-    `);
-    */
+
+    expect(await test.checkTestPasses()).to.be.true;
+
+    // test with WETH - direct USD feed and WBTC - feed via BTC
+    stateAbiEncoded = ethers.utils.defaultAbiCoder.encode(['address', 'address'], [tokens.WETH, tokens.WBTC]);
+    await test.setStateAndCheckTestPasses(stateAbiEncoded);
+
     expect(await test.checkTestPasses()).to.be.true;
   });
 });
