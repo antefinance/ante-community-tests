7a410a3add0392aaf880e8d7234aec69753d1327
kokosro <kokos@kokos.ro>
2023-02-21 06:59:28 +0200

    cleanup

--- contracts/libraries/chainlink/TokenUsdValueMainnet.sol
+++ contracts/libraries/chainlink/TokenUsdValueMainnet.sol
@@ -12,23 +12,41 @@ library TokenUsdValueMainnet {
   address public constant FEED_REGISTRY = 0x47Fb2585D2C56Fe188D0E6ec628a38b74fCeeeDf;
   address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
   
-  function tryGetEthAggregator(address _token) internal view returns (AggregatorV2V3Interface) {
-    try FeedRegistryInterface(FEED_REGISTRY).getFeed(_token, Denominations.ETH) returns (AggregatorV2V3Interface feed) {
+  function tryGetEthAggregator(address _token) 
+    internal 
+    view 
+    returns (AggregatorV2V3Interface) {
+
+    try FeedRegistryInterface(FEED_REGISTRY)
+        .getFeed(_token, Denominations.ETH) 
+        returns (AggregatorV2V3Interface feed) {
       return feed;
     } catch {
       return AggregatorV2V3Interface(address(0));
     }
   }
-  function tryGetBtcAggregator(address _token) internal view returns (AggregatorV2V3Interface) {
-    try FeedRegistryInterface(FEED_REGISTRY).getFeed(_token, Denominations.BTC) returns (AggregatorV2V3Interface feed) {
+  function tryGetBtcAggregator(address _token) 
+    internal 
+    view 
+    returns (AggregatorV2V3Interface) {
+    
+    try FeedRegistryInterface(FEED_REGISTRY)
+        .getFeed(_token, Denominations.BTC) 
+        returns (AggregatorV2V3Interface feed) {
       return feed;
     } catch {
       return AggregatorV2V3Interface(address(0));
     }
   }
 
-  function tryGetUsdAggregator(address _token) internal view returns (AggregatorV2V3Interface) {
-    try FeedRegistryInterface(FEED_REGISTRY).getFeed(_token, Denominations.USD) returns (AggregatorV2V3Interface feed) {
+  function tryGetUsdAggregator(address _token) 
+    internal 
+    view 
+    returns (AggregatorV2V3Interface) {
+
+    try FeedRegistryInterface(FEED_REGISTRY)
+        .getFeed(_token, Denominations.USD) 
+        returns (AggregatorV2V3Interface feed) {
       return feed;
     } catch {
       return AggregatorV2V3Interface(address(0));
@@ -51,16 +69,20 @@ library TokenUsdValueMainnet {
     if(_token == WETH) {
       return true;
     }
-    return hasEthFeed(_token) || hasBtcFeed(_token) || hasUsdFeed(_token);
+    return hasEthFeed(_token) || 
+            hasBtcFeed(_token) || 
+            hasUsdFeed(_token);
   }
   function getValue(address _token, uint256 _amount) internal view returns (uint256) {
     uint256 tokenDecimals = IERC20Metadata(_token).decimals();
     uint256 tokenPrice;
     if(_token == WETH) {
-      tokenPrice = uint256(FeedRegistryInterface(FEED_REGISTRY).latestAnswer(Denominations.ETH, Denominations.USD));
+      tokenPrice = uint256(FeedRegistryInterface(FEED_REGISTRY)
+                          .latestAnswer(Denominations.ETH, Denominations.USD));
     } else {
       if(hasUsdFeed(_token)){
-        tokenPrice = uint256(FeedRegistryInterface(FEED_REGISTRY).latestAnswer(_token, Denominations.USD));
+        tokenPrice = uint256(FeedRegistryInterface(FEED_REGISTRY)
+                            .latestAnswer(_token, Denominations.USD));
       } else if(hasEthFeed(_token)) {
         uint256 tokenPriceEth = uint256(FeedRegistryInterface(FEED_REGISTRY)
                                         .latestAnswer(_token, Denominations.ETH));
