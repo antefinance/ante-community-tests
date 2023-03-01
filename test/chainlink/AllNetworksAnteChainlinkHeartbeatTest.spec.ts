c98f140514964c39443a2d6f13770d8af1e8aceb
kokosro <kokos@kokos.ro>
2023-03-01 10:43:25 +0200

    chainlink tests port to multichain / optimism

--- test/chainlink/AllNetworksAnteChainlinkHeartbeatTest.spec.ts
+++ test/chainlink/AllNetworksAnteChainlinkHeartbeatTest.spec.ts
@@ -1,26 +1,32 @@
 import hre from 'hardhat';
 const { waffle } = hre;
 
-import { AnteChainlinkUpdateTimeTest, AnteChainlinkUpdateTimeTest__factory } from '../../typechain';
+import { AllNetworksAnteChainlinkHeartbeatTest, AllNetworksAnteChainlinkHeartbeatTest__factory } from '../../typechain';
 
 import { evmSnapshot, evmRevert, blockTimestamp } from '../helpers';
 import { expect } from 'chai';
 
-describe('AnteChainlinkUpdateTimeTest', function () {
-  let test: AnteChainlinkUpdateTimeTest;
+describe('AllNetworksAnteChainlinkHeartbeatTest', function () {
+  let test: AllNetworksAnteChainlinkHeartbeatTest;
 
   let globalSnapshotId: string;
 
-  const dataFeeds: Record<string, string[]> = {
+  let priceFeeds24h: Record<string, string[]> = {
     optimisticEthereum: [
       '0x39be70e93d2d285c9e71be7f70fc5a45a7777b14', // AUD / USD
       '0x2ff1eb7d0cec35959f0248e9354c3248c6683d9b', // FLOW / USD
-      '0xCc232dcFAAE6354cE191Bd574108c1aD03f86450', // LINK / USD
-      '0x13e3Ee699D1909E989722E753853AE30b17e08c5', // ETH / USD
     ],
     mainnet: [
       '0x3e7d1eab13ad0104d2750b8863b489d65364e32d', // USDT / USD
       '0x553303d460ee0afb37edff9be42922d8ff63220e', // UNI / USD
+    ],
+  };
+  let priceFeeds1h: Record<string, string[]> = {
+    optimisticEthereum: [
+      '0xCc232dcFAAE6354cE191Bd574108c1aD03f86450', // LINK / USD
+      '0x13e3Ee699D1909E989722E753853AE30b17e08c5', // ETH / USD
+    ],
+    mainnet: [
       '0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419', // ETH / USD
       '0xf4030086522a5beea4988f8ca5b36dbc97bee88c', // BTC / USD,
     ],
@@ -28,13 +34,19 @@ describe('AnteChainlinkUpdateTimeTest', function () {
 
   before(async () => {
     globalSnapshotId = await evmSnapshot();
-    expect(dataFeeds[process.env.NETWORK as string]).to.not.be.undefined;
+
+    expect(priceFeeds24h[process.env.NETWORK as string]).to.not.be.undefined;
+    expect(priceFeeds1h[process.env.NETWORK as string]).to.not.be.undefined;
+
     const [deployer] = waffle.provider.getWallets();
     const factory = (await hre.ethers.getContractFactory(
-      'AnteChainlinkUpdateTimeTest',
+      'AllNetworksAnteChainlinkHeartbeatTest',
       deployer
-    )) as AnteChainlinkUpdateTimeTest__factory;
-    test = await factory.deploy(dataFeeds[process.env.NETWORK as string]);
+    )) as AllNetworksAnteChainlinkHeartbeatTest__factory;
+    test = await factory.deploy(
+      priceFeeds24h[process.env.NETWORK as string],
+      priceFeeds1h[process.env.NETWORK as string]
+    );
     await test.deployed();
   });
 
