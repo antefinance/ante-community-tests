171ef16baf9e21eab13497e77200299681993e72
k2341324 <137750350+k2341324@users.noreply.github.com>
2023-06-26 23:17:15 +0900

    Update AnteAmbientTVLDropTest.sol
    Edited @notice, contract name, corrected checkTestPasses() logic.

--- contracts/ambient/AnteAmbientTVLDropTest.sol
+++ contracts/ambient/AnteAmbientTVLDropTest.sol
@@ -1,36 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0-only
-
 pragma solidity ^0.8.0;
-
 import "../AnteTest.sol";
 import "../interfaces/IERC20.sol";
 
-/// @title AnteAmbientTVLDropTest
-/// @notice Ante Test to check that ETH and USDC in Ambient's Swap Dex remains above 90% of as of deployment
-contract AnteArbitrumPlungeTest is AnteTest("ETH in Arbitrum bridge does NOT drop below 7K") {
-
+/// @title AnteAmbientTVLDropTestexexex
+/// @notice Ante Test that fails if either ETH or USDC balance in Ambient drop -90% from values as of this Ante Test deployment
+contract AnteAmbientPlungeTest is AnteTest("ETH and USDC individual balances in Ambient do NOT drop -90% from this Ante Test deployment") {
     // https://etherscan.io/address/0xAaAaAAAaA24eEeb8d57D431224f73832bC34f688
     address public constant ambientSwapDexAddr = 0xAaAaAAAaA24eEeb8d57D431224f73832bC34f688;
-
     IERC20 public constant usdcToken = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     uint256 public immutable thresholdEth;
     uint256 public immutable thresholdUsdc;
-
     constructor() {
         protocolName = "ambient";
-
         thresholdEth = ambientSwapDexAddr.balance;
         thresholdUsdc = usdcToken.balanceOf(ambientSwapDexAddr);
-
         testedContracts = [ambientSwapDexAddr];
     }
 
-    /// @notice test to check balance of ETH and USDC in Ambient's Swap Dex
+/// @notice test to check balance of ETH and USDC in Ambient Finance
     /// @return true if both ETH and USDC in Ambient's Swap Dex remains above 10% as of the deployment amount
     function checkTestPasses() public view override returns (bool) {
         return (
-            ambientSwapDexAddr.balance < thresholdEth / 10 &&
-            usdcToken.balanceOf(ambientSwapDexAddr) < thresholdUsdc / 10
+             !(ambientSwapDexAddr.balance < thresholdEth / 10 &&
+            usdcToken.balanceOf(ambientSwapDexAddr) < thresholdUsdc / 10)
         );
     }
 }
