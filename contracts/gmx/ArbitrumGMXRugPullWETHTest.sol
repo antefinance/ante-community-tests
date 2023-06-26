d87cb9faabaa8e75b9ca735150d7d67b65d72aa3
deemabot <dima@unitary.com>
2023-06-26 10:27:35 +0200

    fix gmx test function visibility modifier

--- contracts/gmx/ArbitrumGMXRugPullWETHTest.sol
+++ contracts/gmx/ArbitrumGMXRugPullWETHTest.sol
@@ -7,55 +7,45 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 /// @title Check if Arbitrum GMX Vault balance of WETH dropped 99% or more
 /// @notice Ante Test to check if Arbitrum GMX Vault WETH balance dropped 99% or more
-contract ArbitrumGMXRugPullWETHTest is 
-        AnteTest("Arbitrum GMX Rug Pull Test On All Whitelisted Tokens") {
-  
-  // https://arbiscan.io/address/0x489ee077994B6658eAfA855C308275EAd8097C4A#code
-  address constant public GMX_VAULT = 0x489ee077994B6658eAfA855C308275EAd8097C4A;
+contract ArbitrumGMXRugPullWETHTest is AnteTest("Arbitrum GMX Rug Pull Test On All Whitelisted Tokens") {
+    // https://arbiscan.io/address/0x489ee077994B6658eAfA855C308275EAd8097C4A#code
+    address public constant GMX_VAULT = 0x489ee077994B6658eAfA855C308275EAd8097C4A;
 
-  // https://arbiscan.io/address/0x82aF49447D8a07e3bd95BD0d56f35241523fBab1#code
-  IERC20 constant public WETH = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
+    // https://arbiscan.io/address/0x82aF49447D8a07e3bd95BD0d56f35241523fBab1#code
+    IERC20 public constant WETH = IERC20(0x82aF49447D8a07e3bd95BD0d56f35241523fBab1);
 
-  // will hold the last known balance of WETH in GMX Vault
-  uint256 public immutable balanceAtDeployment;
-  
-  constructor() {
-        
-    protocolName = "GMX";
+    // will hold the last known balance of WETH in GMX Vault
+    uint256 public immutable balanceAtDeployment;
 
-    testedContracts = [
-      GMX_VAULT
-    ];
-    
-    balanceAtDeployment = WETH.balanceOf(GMX_VAULT);
-  }
+    constructor() {
+        protocolName = "GMX";
 
-  /// @notice test to check if average balances dropped 99% or more
-  /// @return true if average balances dropped 99% or more
-  function checkTestPasses() external view override returns (bool) {
-  
-      uint256 percentageRemaining = _getBalancePercentageRemaining();
-      // if average balance dropped 99% or more
-      // test fails
-      return percentageRemaining > 1;
+        testedContracts = [GMX_VAULT];
 
-  }
-
-  /// @notice Get the average percentage remaining of WETH in GMX Vault since contract deployment
-  /// @return average percentage remaining of all tokens in GMX Vault
-  function _getBalancePercentageRemaining() internal view returns (uint256) {
-  
-    uint256 currentBalance = WETH.balanceOf(GMX_VAULT);
-
-    if(currentBalance >= balanceAtDeployment){
-      return 100;
+        balanceAtDeployment = WETH.balanceOf(GMX_VAULT);
     }
 
-    uint256 decreaseDelta = balanceAtDeployment - currentBalance;
-    uint256 decreaseDeltaPercentage = decreaseDelta * 100 / balanceAtDeployment;
+    /// @notice test to check if average balances dropped 99% or more
+    /// @return true if average balances dropped 99% or more
+    function checkTestPasses() public view override returns (bool) {
+        uint256 percentageRemaining = _getBalancePercentageRemaining();
+        // if average balance dropped 99% or more
+        // test fails
+        return percentageRemaining > 1;
+    }
 
-    return 100 - decreaseDeltaPercentage;
-    
-  }
+    /// @notice Get the average percentage remaining of WETH in GMX Vault since contract deployment
+    /// @return average percentage remaining of all tokens in GMX Vault
+    function _getBalancePercentageRemaining() internal view returns (uint256) {
+        uint256 currentBalance = WETH.balanceOf(GMX_VAULT);
 
+        if (currentBalance >= balanceAtDeployment) {
+            return 100;
+        }
+
+        uint256 decreaseDelta = balanceAtDeployment - currentBalance;
+        uint256 decreaseDeltaPercentage = (decreaseDelta * 100) / balanceAtDeployment;
+
+        return 100 - decreaseDeltaPercentage;
+    }
 }
