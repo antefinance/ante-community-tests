f148f5a78fecd34251f215fec1987bac57134d31
k2341324 <137750350+k2341324@users.noreply.github.com>
2023-07-06 09:01:36 +0900

    removed unused import from AnteHopEthBridgeRugTest.sol (#219)
    * removed unused import from AnteHopEthBridgeRugTest.sol
    
    * test description fix
    
    * bugfix

--- contracts/hop/AnteHopEthBridgeRugTest.sol
+++ contracts/hop/AnteHopEthBridgeRugTest.sol
@@ -12,12 +12,11 @@
 pragma solidity ^0.8.0;
 
 import "../AnteTest.sol";
-import "@openzeppelin-contracts-old/contracts/token/ERC20/IERC20.sol";
 
 /// @title Hop Ethereum Bridge doesn't rug test on mainnet
 /// @author 0xa0e7Fb16cdE37Ebf2ceD6C89fbAe8780B8497e12
 /// @notice Ante Test to check if Hop Ethereum Bridge rugs
-contract AnteHopEthBridgeRugTest is AnteTest("EOA Avalanche Bridge Doesnt Rug 99% of its Value Test") {
+contract AnteHopEthBridgeRugTest is AnteTest("Hop Ethereum Bridge Doesnt Rug 99% of its Value Test") {
     // https://etherscan.io/address/0xb8901acb165ed027e32754e0ffe830802919727f
     address public constant hopEthBridgeAddr = 0xb8901acB165ed027E32754E0FFe830802919727f;
 
@@ -32,7 +31,7 @@ contract AnteHopEthBridgeRugTest is AnteTest("EOA Avalanche Bridge Doesnt Rug 99
 
     /// @notice test to check value of ether is not rugged
     /// @return true if bridge has more than 1% of assets from when it was deployed
-    function checkTestPasses() external view override returns (bool) {
-        return (etherBalanceAtDeploy / 100) < hopEthBridgeAddr.balance)
+    function checkTestPasses() public view override returns (bool) {
+        return (etherBalanceAtDeploy / 100) < hopEthBridgeAddr.balance;
     }
 }
