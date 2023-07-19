b85975da6bea0e19b882dff6599d38170e522da1
0xHegemon <116603189+0xHegemon@users.noreply.github.com>
2023-07-19 07:09:39 -0500

    Ante PoEM tests (#223)
    * Create AntePoHTest.sol
    
    * memory added for setPreImage
    
    * basic unit test created
    
    * add callback function and sample protocol
    
    * fixed hardhat test
    
    * fix eth reserves bug
    
    * revoke Ante Test address changes
    
    * remove extraneous comments
    
    * proof of private key
    
    * Added PoPK Test using SNARK
    
    * add PoMultiSigPK test
    
    * prepare for Goerli deploy
    
    * cleanup for comments
    
    * prepare for goerli deploy
    
    * fixed empty array compile error
    
    * cleaned up code for Goerli deployment
    
    * deployed AntePoMultiSigPKSnarkTest to Goerli
    
    * Add proof of N-of-4 multisig private key

--- contracts/poe/AntePoMultiSig4PKSnarkTest.sol
+++ contracts/poe/AntePoMultiSig4PKSnarkTest.sol
@@ -3,21 +3,22 @@
 pragma solidity ^0.8.0;
 
 import {AnteTest} from "../AnteTest.sol";
-import "./AntePoMultiSigPKSnarkVerifier.sol";
+import "./AntePoMultiSig4PKSnarkVerifier.sol";
 
 /// @title Nobody knows the private key to one of these public addresses
 /// @notice Ante Test to check that the zk-SNARK verifier has not received a valid zk-SNARK showing a private key to one of these public addresses
-contract AntePoMultiSigPKSnarkTest is AnteTest("Nobody knows a private key for one of these addresses") {
+contract AntePoMultiSig4PKSnarkTest is AnteTest("Nobody knows a private key for one of these 4 addresses") {
     // These are the public addresses that this AnteTest was written for
     address public testAddress1 = 0x66c777464c62F125760f80254257ed8DFccB2921;
     address public testAddress2 = 0x09f1eF9171202A72d0854D421244b2361509dCEd;
     address public testAddress3 = 0x3CF58a049f731E5F278304CAF98B4699129e6A1D;
-    PoMultiSigPKVerifier private verifier;
+    address public testAddress4 = 0x877A50594650D4974f42E830dC7940e715e2920c;
+    PoMultiSig4PKVerifier private verifier;
     address private verifierAddress;
     uint[2] a;
     uint[2][2] b;
     uint[2] c;
-    uint[5] input;
+    uint[6] input;
 
     constructor(address _verifierAddress) {
         protocolName = "";
@@ -25,7 +26,7 @@ contract AntePoMultiSigPKSnarkTest is AnteTest("Nobody knows a private key for o
         testedContracts = [_verifierAddress];
 
         verifierAddress = _verifierAddress;
-        verifier = PoMultiSigPKVerifier(verifierAddress);
+        verifier = PoMultiSig4PKVerifier(verifierAddress);
     }
 
     function checkTestPasses() public view override returns (bool) {
@@ -37,7 +38,7 @@ contract AntePoMultiSigPKSnarkTest is AnteTest("Nobody knows a private key for o
         uint[2] memory _a,
         uint[2][2] memory _b,
         uint[2] memory _c,
-        uint[5] memory _input
+        uint[6] memory _input
     ) public {
         a = _a;
         b = _b;
