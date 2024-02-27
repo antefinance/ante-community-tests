559c8ddc1ecc07d7d8555ac306da50f452b241bc
DeLaLunia <delalunia@ante.finance>
2024-02-26 19:57:15 -0600

    abracadabra tests updated

--- contracts/abracadabra/AnteMIMTest.sol
+++ contracts/abracadabra/AnteMIMTest.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.0;
+pragma solidity ^0.8.0;
 
 import "../interfaces/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
